940a06223682269f904c0c3e42fe29910c324944
Zak <zjones@ion-innovations.com>
2018-02-14 11:43:48 -0500

    solving variable length EntryMethod values and removing defaults

--- QModInstaller/QModPatcher.cs
+++ QModInstaller/QModPatcher.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 
 namespace QModInstaller
@@ -37,31 +38,36 @@ namespace QModInstaller
                 if (mod.Equals(null))
                     continue;
 
-                try
+                var modAssembly = Assembly.LoadFrom(Path.Combine(subDir, mod.AssemblyName));
+
+                if (!string.IsNullOrEmpty(mod.EntryMethod))
                 {
-                    var modAssembly = Assembly.LoadFrom(Path.Combine(subDir, mod.AssemblyName));
-
-                    string entryNamespace = mod.AssemblyName.Replace(".dll", "");
-                    string entryType = "QPatch";
-                    string entryMethod = "Patch";
-
-                    var entryMethodSig = mod.EntryMethod.Split('.');
-                    if (!string.IsNullOrEmpty(mod.EntryMethod) && entryMethodSig[0] != "Namespace")
+                    try
                     {
-                        entryNamespace = entryMethodSig[0];
-                        entryType = entryMethodSig[1];
-                        entryMethod = entryMethodSig[2];
+                        var entryMethodSig = mod.EntryMethod.Split('.');
+                        var entryType = String.Join(".", entryMethodSig.Take(entryMethodSig.Length - 1).ToArray());
+                        var entryMethod = entryMethodSig[entryMethodSig.Length - 1];
+
+                        MethodInfo qPatchMethod = modAssembly.GetType(entryType).GetMethod(entryMethod);
+
+                        if (mod.Enable)
+                            qPatchMethod.Invoke(modAssembly, new object[] { });
                     }
-
-                    MethodInfo qPatchMethod = modAssembly.GetType(entryNamespace + "." + entryType).GetMethod(entryMethod);
-
-                    if (mod.Enable)
-                        qPatchMethod.Invoke(modAssembly, new object[] { });
+                    catch(ArgumentNullException e)
+                    {
+                        Console.WriteLine("QMOD ERR: Could not parse EntryMethod {0} for {1}", mod.AssemblyName, mod.Id);
+                        continue;
+                    }
+                    catch(Exception e)
+                    {
+                        Console.WriteLine("QMOD ERR: Invoking the specified EntryMethod {0} failed for {1}", mod.EntryMethod, mod.Id);
+                        Console.WriteLine(e.Message);
+                        continue;
+                    }
                 }
-                catch (Exception e)
+                else
                 {
-                    Console.WriteLine("QMOD ERR: loading QPatch.Patch method failed: " + e.Message);
-                    continue;
+                    Console.WriteLine("QMOD ERR: Could not open the assembly file specificed: {0}", mod.AssemblyName);
                 }
             }
         }
