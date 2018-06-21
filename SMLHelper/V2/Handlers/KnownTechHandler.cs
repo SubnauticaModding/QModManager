277c8702550f95c6173847170b20159a5898cb54
ahk1221 <ahmed16.ahk@gmail.com>
2018-06-22 00:40:13 +0500

    V2 - Fix merge error

--- SMLHelper/V2/Handlers/KnownTechHandler.cs
+++ SMLHelper/V2/Handlers/KnownTechHandler.cs
@@ -8,15 +8,7 @@
     /// </summary>
     public class KnownTechHandler
     {
-<<<<<<< HEAD
         public static void EditAnalysisTechEntry(TechType techTypeToEdit, TechType techTypeToUnlock, string UnlockMessage = "NotificationBlueprintUnlocked", FMODAsset UnlockSound = null, UnityEngine.Sprite UnlockSprite = null)
-=======
-        /// <summary>
-        /// Adds a custom <see cref="KnownTech.AnalysisTech"/> to add conditions for when an item blueprint unlocks.
-        /// </summary>
-        /// <param name="analysisTech">The analysis tech.</param>        
-        public static void AddToAnalysisTech(KnownTech.AnalysisTech analysisTech)
->>>>>>> ce5afafc92f49516481d51029086814caa1bb908
         {
             KnownTechPatcher.AnalysisTech.Add(new KnownTech.AnalysisTech()
             {
