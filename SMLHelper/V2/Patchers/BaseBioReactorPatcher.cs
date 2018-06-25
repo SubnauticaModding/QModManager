74e38e49f24bdd5d95edd9783762d371d4749a9d
K07H <K07H@users.noreply.github.com>
2018-06-25 19:33:13 +0200

    Added possibility to make a TechType compatible with bioreactors.
    1. Added possibility to make a TechType compatible with bioreactors.
    2. Modified description of the "custom inventory background type" capability to make it more clear.

---
+++ SMLHelper/V2/Patchers/BaseBioReactorPatcher.cs
@@ -0,0 +1,25 @@
+﻿using Harmony;
+using System.Collections.Generic;
+
+namespace SMLHelper.V2.Patchers
+{
+    internal class BaseBioReactorPatcher
+    {
+        #region Internal Fields
+
+        internal static Dictionary<TechType, float> CustomBioreactorCharges = new Dictionary<TechType, float>(TechTypeExtensions.sTechTypeComparer);
+
+        #endregion
+
+        #region Patching
+
+        internal static void Patch(HarmonyInstance harmony)
+        {
+            Utility.PatchDictionary(typeof(BaseBioReactor), "charge", CustomBioreactorCharges);
+
+            Logger.Log("BaseBioReactorPatcher is done.");
+        }
+
+        #endregion
+    }
+}
