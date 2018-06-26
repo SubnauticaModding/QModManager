eed37a00a583394d4927c00ed1c11cb14c5076c5
ahk1221 <ahmed16.ahk@gmail.com>
2018-06-26 19:26:55 +0500

    Update documentation for KnownTechHandler

--- SMLHelper/V2/Handlers/KnownTechHandler.cs
+++ SMLHelper/V2/Handlers/KnownTechHandler.cs
@@ -9,12 +9,14 @@
     /// </summary>
     public class KnownTechHandler
     {
-        public static void EditAnalysisTechEntry(TechType techTypeToEdit, TechType techTypeToUnlock, string UnlockMessage = "NotificationBlueprintUnlocked", FMODAsset UnlockSound = null, UnityEngine.Sprite UnlockSprite = null)
-        {
-            EditAnalysisTechEntry(techTypeToEdit, new List<TechType>() { techTypeToUnlock },
-                UnlockMessage, UnlockSound, UnlockSprite);
-        }
-
+        /// <summary>
+        /// Allows you to define which TechTypes are unlocked when a certain TechType is unlocked, i.e., "analysed".
+        /// </summary>
+        /// <param name="techTypeToEdit">This TechType is the criteria for all of the "unlock TechTypes"; when this TechType is unlocked, so are all the ones in that list</param>
+        /// <param name="techTypesToUnlock">The TechTypes that will be unlocked when "techTypeToEdit" is unlocked.</param>
+        /// <param name="UnlockMessage">The message that shows up on the right when the blueprint is unlocked. </param>
+        /// <param name="UnlockSound">The sound that plays when you unlock the blueprint.</param>
+        /// <param name="UnlockSprite">The sprite that shows up when you unlock the blueprint.</param>
         public static void EditAnalysisTechEntry(TechType techTypeToEdit, IEnumerable<TechType> techTypesToUnlock, string UnlockMessage = "NotificationBlueprintUnlocked", FMODAsset UnlockSound = null, UnityEngine.Sprite UnlockSprite = null)
         {
             KnownTechPatcher.AnalysisTech.Add(new KnownTech.AnalysisTech()
