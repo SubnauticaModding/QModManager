118a49d7880a0e15793be44585e971434b62448b
PrimeSonic <prime.sonic@gmail.com>
2018-06-27 19:50:08 -0400

    Added new methods to SpriteHandler to allow a sprite to be registered without needing to instantiate a new ModSprite separately.

--- SMLHelper/Handlers/SpriteHandler.cs
+++ SMLHelper/Handlers/SpriteHandler.cs
@@ -15,5 +15,47 @@
         {
             ModSprite.Sprites.Add(sprite);
         }
+
+        /// <summary>
+        /// Registers a new sprite to the game.
+        /// </summary>
+        /// <param name="type">The techtype paired to this sprite.</param>
+        /// <param name="sprite">The sprite to be added.</param>
+        public static void RegisterSprite(TechType type, Atlas.Sprite sprite)
+        {
+            ModSprite.Sprites.Add(new ModSprite(type, sprite));
+        }
+
+        /// <summary>
+        /// Registers a new sprite to the game.
+        /// </summary>
+        /// <param name="group">The sprite group.</param>
+        /// <param name="id">The sprite internal identifier.</param>
+        /// <param name="sprite">The sprite to be added.</param>
+        public static void RegisterSprite(SpriteManager.Group group, string id, Atlas.Sprite sprite)
+        {
+            ModSprite.Sprites.Add(new ModSprite(group, id, sprite));
+        }
+
+        /// <summary>
+        /// Registers a new sprite to the game.
+        /// </summary>
+        /// <param name="group">The sprite group this sprite will be added to.</param>
+        /// <param name="id">The sprite internal identifier.</param>
+        /// <param name="sprite">The sprite to be added.</param>
+        public static void RegisterSprite(SpriteManager.Group group, string id, UnityEngine.Sprite sprite)
+        {
+            ModSprite.Sprites.Add(new ModSprite(group, id, sprite));
+        }
+
+        /// <summary>
+        /// Registers a new sprite to the game.
+        /// </summary>
+        /// <param name="type">The techtype paired to this sprite.</param>
+        /// <param name="sprite">The sprite to be added.</param>
+        public static void RegisterSprite(TechType type, UnityEngine.Sprite sprite)
+        {
+            ModSprite.Sprites.Add(new ModSprite(type, sprite));
+        }
     }
 }
