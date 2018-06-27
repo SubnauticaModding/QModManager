524cf0c20d0fbfdfcb223eca11174114b17ecb9f
AlexejheroYTB <alexejhero@gmail.com>
2018-06-27 18:10:59 +0300

    More ReflectionHelper changes

--- SMLHelper/V2/Util/ReflectionHelper.cs
+++ SMLHelper/V2/Util/ReflectionHelper.cs
@@ -23,8 +23,9 @@
         /// <exception cref="NotSupportedException"/>
         /// <exception cref="FieldAccessException"/>
         /// <exception cref="ArgumentException"/>
-        public static object GetInstanceField<classInstance>(classInstance instance, string field, BindingFlags bindingFlags = BindingFlags.NonPublic | BindingFlags.Instance) where classInstance : class
+        public static object GetInstanceField<classInstance>(this classInstance instance, string field, BindingFlags bindingFlags = BindingFlags.NonPublic | BindingFlags.Instance) where classInstance : class
             => typeof(classInstance).GetField(field, bindingFlags).GetValue(instance);
+
         /// <summary>
         /// Sets the value of a field from an instance of a class using reflection
         /// <para/> Extension from <see langword="AlexejheroYTB.Utilities.Extensions"/>
@@ -38,8 +39,43 @@
         /// <exception cref="FieldAccessException"/>
         /// <exception cref="TargetException"/>
         /// <exception cref="ArgumentException"/>
-        public static void SetInstanceField<classInstance>(classInstance instance, string field, object newValue, BindingFlags bindingFlags = BindingFlags.NonPublic | BindingFlags.Instance) where classInstance : class
+        public static void SetInstanceField<classInstance>(this classInstance instance, string field, object newValue, BindingFlags bindingFlags = BindingFlags.NonPublic | BindingFlags.Instance) where classInstance : class
             => typeof(classInstance).GetField(field, bindingFlags).SetValue(instance, newValue);
+
+
+        /// <summary>
+        /// Gets the value of a field from a static class using reflection
+        /// <para/> Extension from <see langword="AlexejheroYTB.Utilities.Extensions"/>
+        /// </summary>
+        /// <typeparam name="class">The class to get the value from</typeparam>
+        /// <param name="_class">An instance of the class. Not actually used for anything, just to make it an extension method</param>
+        /// <param name="field">The name of the field</param>
+        /// <param name="bindingFlags">The <see cref="BindingFlags"/> to use with <see cref="Type.GetField(string, BindingFlags)"/></param>
+        /// <returns>The value of field from the instance of the class</returns>
+        /// <exception cref="ArgumentNullException"/>
+        /// <exception cref="TargetException"/>
+        /// <exception cref="NotSupportedException"/>
+        /// <exception cref="FieldAccessException"/>
+        /// <exception cref="ArgumentException"/>
+        public static object GetStaticField<@class>(this @class _class, string field, BindingFlags bindingFlags = BindingFlags.NonPublic | BindingFlags.Static) where @class : class
+            => typeof(@class).GetField(field, bindingFlags).GetValue(null);
+        
+        /// <summary>
+        /// Sets the value of a field from a static class using reflection
+        /// <para/> Extension from <see langword="AlexejheroYTB.Utilities.Extensions"/>
+        /// </summary>
+        /// <typeparam name="class">The class to get the value from</typeparam>
+        /// <param name="_class">An instance of the class. Not actually used for anything, just to make it an extension method</param>
+        /// <param name="field">The name of the field</param>
+        /// <param name="newValue">The new value to set</param>
+        /// <param name="bindingFlags">The <see cref="BindingFlags"/> to use with <see cref="Type.GetField(string, BindingFlags)"/></param>
+        /// <exception cref="ArgumentNullException"/>
+        /// <exception cref="FieldAccessException"/>
+        /// <exception cref="TargetException"/>
+        /// <exception cref="ArgumentException"/>
+        public static void SetStaticField<@class>(this @class _class, string field, object newValue, BindingFlags bindingFlags = BindingFlags.NonPublic | BindingFlags.Static) where @class : class
+            => typeof(@class).GetField(field, bindingFlags).SetValue(null, newValue);
+
         /// <summary>
         /// Gets the value of a field from a static class using reflection
         /// <para/> Extension from <see langword="AlexejheroYTB.Utilities.Extensions"/>
@@ -55,6 +91,7 @@
         /// <exception cref="ArgumentException"/>
         public static object GetStaticField<@class>(string field, BindingFlags bindingFlags = BindingFlags.NonPublic | BindingFlags.Static) where @class : class
             => typeof(@class).GetField(field, bindingFlags).GetValue(null);
+
         /// <summary>
         /// Sets the value of a field from a static class using reflection
         /// <para/> Extension from <see langword="AlexejheroYTB.Utilities.Extensions"/>
@@ -69,5 +106,6 @@
         /// <exception cref="ArgumentException"/>
         public static void SetStaticField<@class>(string field, object newValue, BindingFlags bindingFlags = BindingFlags.NonPublic | BindingFlags.Static) where @class : class
             => typeof(@class).GetField(field, bindingFlags).SetValue(null, newValue);
+
     }
 }
