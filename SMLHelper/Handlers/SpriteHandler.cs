d08bf0e680bf36494555e851f465c8d2b66d5f50
PrimeSonic <prime.sonic@gmail.com>
2018-06-28 19:32:30 -0400

    2.0 SpriteHandler - Added new RegisterSprite methods to allow sprites to be added from image files. - Thank you Randy

--- SMLHelper/Handlers/SpriteHandler.cs
+++ SMLHelper/Handlers/SpriteHandler.cs
@@ -1,6 +1,10 @@
 ﻿namespace SMLHelper.V2.Handlers
 {
+    using System;
+    using System.IO;
     using Assets;
+    using UnityEngine;
+    using Logger = V2.Logger;
 
     /// <summary>
     /// A handler class for adding custom sprites into the game.
@@ -43,7 +47,7 @@
         /// <param name="group">The sprite group this sprite will be added to.</param>
         /// <param name="id">The sprite internal identifier.</param>
         /// <param name="sprite">The sprite to be added.</param>
-        public static void RegisterSprite(SpriteManager.Group group, string id, UnityEngine.Sprite sprite)
+        public static void RegisterSprite(SpriteManager.Group group, string id, Sprite sprite)
         {
             ModSprite.Sprites.Add(new ModSprite(group, id, sprite));
         }
@@ -53,9 +57,84 @@
         /// </summary>
         /// <param name="type">The techtype paired to this sprite.</param>
         /// <param name="sprite">The sprite to be added.</param>
-        public static void RegisterSprite(TechType type, UnityEngine.Sprite sprite)
+        public static void RegisterSprite(TechType type, Sprite sprite)
         {
             ModSprite.Sprites.Add(new ModSprite(type, sprite));
+        }
+
+        /// <summary>
+        /// Registers a new sprite to the game.
+        /// </summary>
+        /// <param name="type">The techtype paired to this sprite.</param>
+        /// <param name="filePathToImage">The file path to image to be converted into a sprite.</param>
+        /// <param name="format"><para>The texture format. By default, this uses <see cref="TextureFormat.BC7" />.</para>
+        /// <para>https://docs.unity3d.com/ScriptReference/TextureFormat.BC7.html</para>
+        /// <para>Don't change this unless you really know what you're doing.</para></param>
+        /// <seealso cref="LoadTextureFromFile(string, TextureFormat)" />
+        public static void RegisterSprite(TechType type, string filePathToImage, TextureFormat format = TextureFormat.BC7)
+        {
+            Texture2D textureFromImage = LoadTextureFromFile(filePathToImage, format);
+
+            if (textureFromImage == null)
+                return;
+            
+            var sprite = new Atlas.Sprite(textureFromImage);
+
+            RegisterSprite(type, sprite);
+        }
+
+        /// <summary>
+        /// Registers a new sprite to the game.
+        /// </summary>
+        /// <param name="group">The sprite group.</param>
+        /// <param name="id">The sprite internal identifier.</param>
+        /// <param name="filePathToImage">The file path to image.</param>
+        /// <param name="format"><para>The texture format. By default, this uses <see cref="TextureFormat.BC7" />.</para>
+        /// <para>https://docs.unity3d.com/ScriptReference/TextureFormat.BC7.html</para>
+        /// <para>Don't change this unless you really know what you're doing.</para></param>
+        /// <seealso cref="LoadTextureFromFile(string, TextureFormat)" />
+        public static void RegisterSprite(SpriteManager.Group group, string id, string filePathToImage, TextureFormat format = TextureFormat.BC7)
+        {
+            Texture2D textureFromImage = LoadTextureFromFile(filePathToImage, format);
+
+            if (textureFromImage == null)
+                return;
+
+            var sprite = new Atlas.Sprite(textureFromImage);
+
+            RegisterSprite(group, id, sprite);
+        }
+        
+        /// <summary>
+        /// Creates a new <see cref="Texture2D" /> from an image file.
+        /// </summary>
+        /// <param name="filePathToImage">The path to the image file.</param>
+        /// <param name="format">
+        /// <para>The texture format. By default, this uses <see cref="TextureFormat.BC7" />.</para>
+        /// <para>https://docs.unity3d.com/ScriptReference/TextureFormat.BC7.html</para>
+        /// <para>Don't change this unless you really know what you're doing.</para>
+        /// </param>
+        /// <returns>Will return a new <see cref="Texture2D"/> instance if the file exists; Otherwise returns null.</returns>
+        /// <remarks>
+        /// Ripped from: https://github.com/RandyKnapp/SubnauticaModSystem/blob/master/SubnauticaModSystem/Common/Utility/ImageUtils.cs
+        /// </remarks>
+        public static Texture2D LoadTextureFromFile(string filePathToImage, TextureFormat format = TextureFormat.BC7)
+        {
+            if (File.Exists(filePathToImage))
+            {
+                byte[] imageBytes = File.ReadAllBytes(filePathToImage);
+                Texture2D texture2D = new Texture2D(2, 2, format, false);
+                if (texture2D.LoadImage(imageBytes))
+                {
+                    return texture2D;
+                }
+            }
+            else
+            {
+                Logger.Log("ERROR on LoadTextureFromFile call. File not found at " + filePathToImage);
+            }
+
+            return null;
         }
     }
 }
