bde809f06090a2d953ce0db13a9e957eb1212e99
ahk1221 <ahmed16.ahk@gmail.com>
2018-07-03 14:27:41 +0500

    Add LanguageHandler

---
+++ SMLHelper/Handlers/LanguageHandler.cs
@@ -0,0 +1,17 @@
+﻿namespace SMLHelper.V2.Handlers
+{
+    using Patchers;
+
+    public class LanguageHandler
+    {
+        /// <summary>
+        /// Allows you to edit/add a language entry into the game.
+        /// </summary>
+        /// <param name="lineId">The ID of the entry, this is what is used to get the actual text.</param>
+        /// <param name="text">The actual text related to the entry.</param>
+        public static void EditLanguageLine(string lineId, string text)
+        {
+            LanguagePatcher.customLines[lineId] = text;
+        }
+    }
+}
