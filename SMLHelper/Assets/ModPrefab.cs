5fcb290318a7831ce4dfd47540b91ccb27c0bc7f
K07H <K07H@users.noreply.github.com>
2018-11-10 17:15:34 +0100

    Revert "Fix conflict"
    This reverts commit ed2aba6fc388e6447266532cefe37a58ff4dd9ee.

--- SMLHelper/Assets/ModPrefab.cs
+++ SMLHelper/Assets/ModPrefab.cs
@@ -3,53 +3,38 @@
     using System.Collections.Generic;
     using UnityEngine;
     using MonoBehaviours;
-    using System;
 
     /// <summary>
     /// The abstract class to inherit when you want to add new PreFabs into the game.
     /// </summary>
     public abstract class ModPrefab
     {
-        private static readonly Dictionary<string, ModPrefab> FileNameDictionary = new Dictionary<string, ModPrefab>(StringComparer.InvariantCultureIgnoreCase);
-        private static readonly Dictionary<string, ModPrefab> ClassIdDictionary = new Dictionary<string, ModPrefab>(StringComparer.InvariantCultureIgnoreCase);
-        private static readonly List<ModPrefab> PreFabsList = new List<ModPrefab>();
-
-        internal static void Add(ModPrefab prefab)
-        {
-            FileNameDictionary.Add(prefab.PrefabFileName, prefab);
-            ClassIdDictionary.Add(prefab.ClassID, prefab);
-            PreFabsList.Add(prefab);
-        }
-
-        internal static IEnumerable<ModPrefab> Prefabs => PreFabsList;
-        internal static bool TryGetFromFileName(string classId, out ModPrefab prefab) => FileNameDictionary.TryGetValue(classId, out prefab);
-        internal static bool TryGetFromClassId(string classId, out ModPrefab prefab) => ClassIdDictionary.TryGetValue(classId, out prefab);
+        internal static List<ModPrefab> Prefabs = new List<ModPrefab>();
 
         /// <summary>
         /// The class identifier used for the <see cref="PrefabIdentifier" /> component whenever applicable.
         /// </summary>
-        public string ClassID { get; protected set; }
+        public readonly string ClassID;
 
         /// <summary>
         /// Name of the prefab file.
         /// </summary>
-        public string PrefabFileName { get; protected set; }
+        public readonly string PrefabFileName;
 
         /// <summary>
         /// The techtype of the corresponding item.
-        /// Used for the <see cref="Fixer" />, <see cref="TechTag" />, and <see cref="Constructable" /> components whenever applicable.
+        /// Used for the <see cref="TechTypeFixer" />, <see cref="TechTag" />, and <see cref="Constructable" /> components whenever applicable.
         /// </summary>
-        public TechType TechType { get; protected set; }
+        public readonly TechType TechType;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="ModPrefab" /> class.
         /// </summary>
         /// <param name="classId">The class identifier used for the <see cref="PrefabIdentifier" /> component whenever applicable.</param>
         /// <param name="prefabFileName">Name of the prefab file.</param>
-        /// <param name="techType">The techtype of the corresponding item. 
-        /// Used for the <see cref="Fixer" />, <see cref="TechTag" />, and <see cref="Constructable" /> components whenever applicable.
-        /// Can also be set later in the constructor if it is not yet provided.</param>
-        protected ModPrefab(string classId, string prefabFileName, TechType techType = TechType.None)
+        /// <param name="techType">The techtype of the corresponding item.
+        /// Used for the <see cref="TechTypeFixer" />, <see cref="TechTag" />, and <see cref="Constructable" /> components whenever applicable.</param>
+        protected ModPrefab(string classId, string prefabFileName, TechType techType)
         {
             ClassID = classId;
             PrefabFileName = prefabFileName;
@@ -67,29 +52,25 @@
 
             go.transform.position = new Vector3(-5000, -5000, -5000);
             go.name = ClassID;
+            go.AddComponent<Fixer>().techType = TechType;
 
-            if(TechType != TechType.None)
-            {
-                go.AddComponent<Fixer>().techType = TechType;
-
-                /* Make sure prefab doesn't get cleared when quiting game to menu. */
-                SceneCleanerPreserve scp = go.AddComponent<SceneCleanerPreserve>();
-                scp.enabled = true;
-
-                if (go.GetComponent<TechTag>() != null)
-                {
-                    go.GetComponent<TechTag>().type = TechType;
-                }
-
-                if (go.GetComponent<Constructable>() != null)
-                {
-                    go.GetComponent<Constructable>().techType = TechType;
-                }
-            }
+            /* Make sure prefab doesn't get cleared when quiting game to menu. */
+            SceneCleanerPreserve scp = go.AddComponent<SceneCleanerPreserve>();
+            scp.enabled = true;
 
             if (go.GetComponent<PrefabIdentifier>() != null)
             {
                 go.GetComponent<PrefabIdentifier>().ClassId = ClassID;
+            }
+
+            if (go.GetComponent<TechTag>() != null)
+            {
+                go.GetComponent<TechTag>().type = TechType;
+            }
+
+            if (go.GetComponent<Constructable>() != null)
+            {
+                go.GetComponent<Constructable>().techType = TechType;
             }
 
             return go;
