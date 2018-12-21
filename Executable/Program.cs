6a3a531d2af69e19f42589aa34a8677d660d4cca
AlexejheroYTB <alexejhero@gmail.com>
2018-12-21 15:11:47 +0200

    Updated console program

--- Executable/Program.cs
+++ Executable/Program.cs
@@ -3,100 +3,161 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 
 namespace QModManager
 {
-    class Program
-    {
-        static void Main(string[] args)
+    public enum Action
         {
-            var parsedArgs = new Dictionary<string, string>();
-            bool forceInstall = false;
-            bool forceUninstall = false;
+            Install,
+            Uninstall,
+            RunByUser
+        }
 
-            foreach (var arg in args)
+    public static class ConsoleExecutable
+    {
+        public static Action action = Action.RunByUser;
+
+        public static void Main(string[] args)
+        {
+            try
             {
-                if (arg.Contains("="))
-                {
-                    parsedArgs = args.Select(s => s.Split(new[] { '=' }, 1)).ToDictionary(s => s[0], s => s[1]);
-                }
-                else if (arg.StartsWith("-"))
-                {
-                    if (arg == "-i")
-                        forceInstall = true;
+                var parsedArgs = new Dictionary<string, string>();
 
-                    if (arg == "-u")
-                        forceUninstall = true;
-                }
-            }
-
-            //string SubnauticaDirectory = @"C:\Program Files (x86)\Steam\steamapps\common\Subnautica";
-            string SubnauticaDirectory = Path.Combine(Environment.CurrentDirectory, @"..\..");
-            string ManagedDirectory = Environment.CurrentDirectory;
-
-            if (parsedArgs.Keys.Contains("SubnauticaDirectory"))
-                SubnauticaDirectory = parsedArgs["SubnauticaDirectory"];
-
-            QModInjector injector = new QModInjector(SubnauticaDirectory, ManagedDirectory);
-
-            bool isInjected = injector.IsPatcherInjected();
-            if (forceInstall)
-            {
-                if (!isInjected)
+                foreach (var arg in args)
                 {
-					Console.WriteLine("Installing QMods...");
-					injector.Inject();
-                }
-                else
-                {
-                    Console.WriteLine("Tried to Force Install, was already injected. Skipping installation.");
-                    return;
-                }
-            }
-            else if (forceUninstall)
-            {
-                if (isInjected)
-                {
-					Console.WriteLine("Uninstalling QMods...");
-					injector.Remove();
-                }
-                else
-                {
-                    Console.WriteLine("Tried to Force Uninstall, was not injected. Skipping uninstallation.");
-                    return;
-                }
-            }
-            else
-            {
-                if (!injector.IsPatcherInjected())
-                {
-                    Console.WriteLine("No patch detected, type 'yes' to install: ");
-                    string consent = Console.ReadLine().Replace("'", "");
-                    if (consent == "yes" || consent == "YES")
+                    if (arg.Contains("="))
                     {
-                        if (injector.Inject())
-                            Console.WriteLine("QMods was installed!");
-                        else
-                            Console.WriteLine("Error installed QMods. Please contact us on Discord");
+                        parsedArgs = args.Select(s => s.Split(new[] { '=' }, 1)).ToDictionary(s => s[0], s => s[1]);
+                    }
+                    else if (arg == "-i") action = Action.Install;
+                }
+
+                string directory = Path.Combine(Environment.CurrentDirectory, @"..\..");
+                string managedDirectory = Environment.CurrentDirectory;
+
+                if (!File.Exists(managedDirectory + "/Assembly-CSharp.dll"))
+                {
+                    Console.WriteLine("Could not find the assembly file.");
+                    Console.WriteLine("Please make sure you have installed QModManager in the right folder.");
+                    Console.WriteLine("If the problem persists, open a bug report on NexusMods or an issue on GitHub");
+                    Console.WriteLine();
+                    Console.WriteLine("Press any key to exit...");
+                    Console.ReadKey();
+                    Environment.Exit(1);
+                }
+
+                if (Directory.GetFiles(directory, "*Subnautica*.exe", SearchOption.TopDirectoryOnly).Length <= 0)
+                {
+                    Console.WriteLine("Could not find any game to patch!");
+                    Console.WriteLine("An assembly file was found, but no executable was detected.");
+                    Console.WriteLine("Please make sure you have installed QModManager in the right folder.");
+                    Console.WriteLine("If the problem persists, open a bug report on NexusMods or an issue on GitHub");
+                    Console.WriteLine();
+                    Console.WriteLine("Press any key to exit...");
+                    Console.ReadKey();
+                    Environment.Exit(1);
+                }
+
+#warning TODO: Improve injector code. It's 2018 out there...
+                QModInjector injector = new QModInjector(directory, managedDirectory);
+
+                bool isInjected = injector.IsInjected();
+
+                if (action == Action.Install)
+                {
+                    if (!isInjected)
+                    {
+                        Console.WriteLine("Installing QModManager...");
+                        injector.Inject();
+                    }
+                    else
+                    {
+                        Console.WriteLine("QModManager is already installed!");
+                        Console.WriteLine("Skipping installation");
+                        Console.WriteLine();
+                        Console.WriteLine("Press any key to exit...");
+                        Console.ReadKey();
+                        Environment.Exit(0);
+                    }
+                }
+                else if (action == Action.Uninstall)
+                {
+                    if (isInjected)
+                    {
+                        Console.WriteLine("Uninstalling QModManager...");
+                        injector.Remove();
+                    }
+                    else
+                    {
+                        Console.WriteLine("QModManager is already uninstalled!");
+                        Console.WriteLine("Skipping uninstallation");
+                        Console.WriteLine();
+                        Console.WriteLine("Press any key to exit...");
+                        Console.ReadKey();
+                        Environment.Exit(0);
                     }
                 }
                 else
                 {
-                    Console.WriteLine("Patch already installed! Type 'yes' to remove: ");
-                    string consent = Console.ReadLine().Replace("'", "");
-                    if (consent == "yes" || consent == "YES")
+                    if (!isInjected)
                     {
-                        if (injector.Remove())
-                            Console.WriteLine("QMods was removed!");
-                        else
-                            Console.WriteLine("Error removing QMods. Please contact us on Discord");
+                        Console.Write("No patch detected, install? [Y/N] > ");
+                        var key = Console.ReadKey().Key;
+                        Console.WriteLine();
+                        if (key == ConsoleKey.Y)
+                        {
+                            Console.WriteLine("Installing QModManager...");
+                            injector.Inject();
+                        }
+                        else if (key == ConsoleKey.N)
+                        {
+                            Console.WriteLine("Press any key to exit...");
+                            Console.ReadKey();
+                            Environment.Exit(0);
+                        }
+                    }
+                    else
+                    {
+                        Console.Write("Patch installed, remove? [Y/N] > ");
+                        var key = Console.ReadKey().Key;
+                        Console.WriteLine();
+                        if (key == ConsoleKey.Y)
+                        {
+                            Console.Write("Uninstalling QModManager...");
+                            injector.Remove();
+                        }
+                        else if (key == ConsoleKey.N)
+                        {
+                            Console.WriteLine("Press any key to exit...");
+                            Console.ReadKey();
+                            Environment.Exit(0);
+                        }
                     }
                 }
-
-                Console.WriteLine("Press any key to exit ...");
-
-                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                ExceptionUtils.ParseException(e);
+                Environment.Exit(2);
             }
         }
+
+        #region Disable exit
+
+        public static void DisableExit()
+        {
+            DisableExitButton();
+            Console.CancelKeyPress += CancelKeyPress;
+            Console.TreatControlCAsInput = true;
+        }
+
+        public static void DisableExitButton() => EnableMenuItem(GetSystemMenu(GetConsoleWindow(), false), 0xF060, 0x1);
+        private static void CancelKeyPress(object sender, ConsoleCancelEventArgs e) => e.Cancel = true;
+        [DllImport("user32.dll")] public static extern int EnableMenuItem(IntPtr tMenu, int targetItem, int targetStatus);
+        [DllImport("user32.dll")] public static extern IntPtr GetSystemMenu(IntPtr hWnd, bool bRevert);
+        [DllImport("kernel32.dll", ExactSpelling = true)] public static extern IntPtr GetConsoleWindow();
+
+        #endregion
     }
 }
