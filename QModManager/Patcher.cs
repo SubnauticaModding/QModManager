b5c9535104c9c0f6088e3ba3220adf81e6675c9d
AlexejheroYTB <alexejhero@gmail.com>
2018-12-21 16:30:59 +0200

    Update Patcher.cs

--- QModManager/Patcher.cs
+++ QModManager/Patcher.cs
@@ -9,19 +9,37 @@ namespace QModManager
 {
     public class QModPatcher
     {
-        private static string qModBaseDir = Path.Combine(Environment.CurrentDirectory, "QMods");
-        private static List<QMod> loadedMods = new List<QMod>();
-        private static bool patched = false;
+        internal static string QModBaseDir = Environment.CurrentDirectory.Contains("system32") && Environment.CurrentDirectory.Contains("Windows") ? "ERR" : Path.Combine(Environment.CurrentDirectory, "QMods");
+        internal static List<QMod> loadedMods = new List<QMod>();
+        internal static bool patched = false;
 
         public static void Patch()
         {
+            try
+            {
+                LoadMods();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("EXCEPTION CAUGHT!");
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+                if (e.InnerException != null)
+                {
+                    Console.WriteLine(e.InnerException.Message);
+                    Console.WriteLine(e.InnerException.StackTrace);
+                }
+            }
+        }
+        internal static void LoadMods()
+        {
             AppDomain.CurrentDomain.AssemblyResolve += (sender, args) =>
             {
-                var allDlls = new DirectoryInfo(qModBaseDir).GetFiles("*.dll", SearchOption.AllDirectories);
-                foreach(var dll in allDlls)
+                var allDlls = new DirectoryInfo(QModBaseDir).GetFiles("*.dll", SearchOption.AllDirectories);
+                foreach (var dll in allDlls)
                 {
-                    Console.WriteLine(Path.GetFileNameWithoutExtension(dll.Name) + " " + args.Name);
-                    if(args.Name.Contains(Path.GetFileNameWithoutExtension(dll.Name)))
+                    WriteLine(Path.GetFileNameWithoutExtension(dll.Name) + " " + args.Name);
+                    if (args.Name.Contains(Path.GetFileNameWithoutExtension(dll.Name)))
                     {
                         return Assembly.LoadFrom(dll.FullName);
                     }
@@ -31,21 +49,40 @@ namespace QModManager
             };
 
             if (patched) return;
-
             patched = true;
 
-            if (!Directory.Exists(qModBaseDir))
+            if (!Directory.Exists(QModBaseDir))
             {
-                Console.WriteLine("QMOD ERR: QMod directory was not found");
-                Directory.CreateDirectory(qModBaseDir);
-                Console.WriteLine("QMOD INFO: Creaated QMod directory at {0}", qModBaseDir);
+                Console.WriteLine("QMods directory was not found! Creating...");
+                if (QModBaseDir == "ERR")
+                {
+                    Console.WriteLine("There was an error creating the QMods directory");
+                    Console.WriteLine("Please make sure that you ran Subnautica from Steam");
+                }
+                try
+                {
+                    Directory.CreateDirectory(QModBaseDir);
+                    Console.WriteLine("QMods directory created successfully!");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("EXCEPTION CAUGHT!");
+                    Console.WriteLine(e.Message);
+                    Console.WriteLine(e.StackTrace);
+                    if (e.InnerException != null)
+                    {
+                        Console.WriteLine("INNER EXCEPTION:");
+                        Console.WriteLine(e.InnerException.Message);
+                        Console.WriteLine(e.InnerException.StackTrace);
+                    }
+                }
                 return;
             }
 
-            var subDirs = Directory.GetDirectories(qModBaseDir);
-            var lastMods = new List<QMod>();
+            var subDirs = Directory.GetDirectories(QModBaseDir);
             var firstMods = new List<QMod>();
             var otherMods = new List<QMod>();
+            var lastMods = new List<QMod>();
 
             foreach (var subDir in subDirs)
             {
@@ -53,20 +90,19 @@ namespace QModManager
 
                 if (!File.Exists(jsonFile))
                 {
-                    Console.WriteLine("QMOD ERR: Mod is missing a mod.json file");
+                    Console.WriteLine($"ERROR! No \"mod.json\" file found in folder \"{subDir}\"");
                     File.WriteAllText(jsonFile, JsonConvert.SerializeObject(new QMod()));
-                    Console.WriteLine("QMOD INFO: A template for mod.json was generated at {0}", jsonFile);
+                    Console.WriteLine("A template file was created");
                     continue;
                 }
 
                 QMod mod = QMod.FromJsonFile(Path.Combine(subDir, "mod.json"));
 
-                if (mod.Equals(null)) // QMod.FromJsonFile will throw parser errors
-                    continue;
+                if (mod == null) continue;
 
-                if (mod.Enable.Equals(false))
+                if (mod.Enable == false)
                 {
-                    Console.WriteLine("QMOD WARN: {0} is disabled via config, skipping", mod.DisplayName);
+                    Console.WriteLine($"{mod.DisplayName} is disabled via config, skipping");
                     continue;
                 }
 
@@ -74,19 +110,19 @@ namespace QModManager
 
                 if (!File.Exists(modAssemblyPath))
                 {
-                    Console.WriteLine("QMOD ERR: No matching dll found at {0} for {1}", modAssemblyPath, mod.Id);
+                    Console.WriteLine($"ERROR! No matching dll found at \"{modAssemblyPath}\" for mod \"{mod.DisplayName}\"");
                     continue;
                 }
 
-                mod.loadedAssembly = Assembly.LoadFrom(modAssemblyPath);
-                mod.modAssemblyPath = modAssemblyPath;
+                mod.LoadedAssembly = Assembly.LoadFrom(modAssemblyPath);
+                mod.ModAssemblyPath = modAssemblyPath;
 
                 if (mod.Priority.Equals("Last"))
                 {
                     lastMods.Add(mod);
                     continue;
                 }
-                else if(mod.Priority.Equals("First"))
+                else if (mod.Priority.Equals("First"))
                 {
                     firstMods.Add(mod);
                     continue;
@@ -98,60 +134,89 @@ namespace QModManager
                 }
             }
 
-            foreach(var mod in firstMods)
+            foreach (var mod in firstMods)
             {
                 if (mod != null)
                     loadedMods.Add(LoadMod(mod));
             }
 
-            foreach(var mod in otherMods)
+            foreach (var mod in otherMods)
             {
                 if (mod != null)
                     loadedMods.Add(LoadMod(mod));
             }
 
-            foreach(var mod in lastMods)
+            foreach (var mod in lastMods)
             {
-                if(mod != null)
+                if (mod != null)
                     loadedMods.Add(LoadMod(mod));
+            }
+
+            List<QMod> mods = firstMods.Union(otherMods).Union(lastMods).ToList();
+            mods.Sort();
+
+            Console.WriteLine(" ");
+            Console.WriteLine("Installed mods:");
+
+            foreach (var mod in mods)
+            {
+                Console.WriteLine($"- {mod.DisplayName} ({mod.Id})");
             }
         }
 
-        private static QMod LoadMod(QMod mod)
+        internal static QMod LoadMod(QMod mod)
         {
             if (mod == null) return null;
 
             if (string.IsNullOrEmpty(mod.EntryMethod))
             {
-                Console.WriteLine("QMOD ERR: No EntryMethod specified for {0}", mod.Id);
+                Console.WriteLine($"ERROR! No EntryMethod specified for mod {mod.DisplayName}");
             }
             else
             {
                 try
                 {
                     var entryMethodSig = mod.EntryMethod.Split('.');
-                    var entryType = String.Join(".", entryMethodSig.Take(entryMethodSig.Length - 1).ToArray());
+                    var entryType = string.Join(".", entryMethodSig.Take(entryMethodSig.Length - 1).ToArray());
                     var entryMethod = entryMethodSig[entryMethodSig.Length - 1];
 
-                    MethodInfo qPatchMethod = mod.loadedAssembly.GetType(entryType).GetMethod(entryMethod);
-                    qPatchMethod.Invoke(mod.loadedAssembly, new object[] { });
+                    MethodInfo qPatchMethod = mod.LoadedAssembly.GetType(entryType).GetMethod(entryMethod);
+                    qPatchMethod.Invoke(mod.LoadedAssembly, new object[] { });
                 }
                 catch (ArgumentNullException e)
                 {
-                    Console.WriteLine("QMOD ERR: Could not parse EntryMethod {0} for {1}", mod.AssemblyName, mod.Id);
-                    Console.WriteLine(e.InnerException.Message);
+                    Console.WriteLine($"ERROR! Could not parse entry method {mod.AssemblyName} for mod {mod.DisplayName}");
+                    Console.WriteLine(e.Message);
+                    Console.WriteLine(e.StackTrace);
+                    if (e.InnerException != null)
+                    {
+                        Console.WriteLine(e.InnerException.Message);
+                        Console.WriteLine(e.InnerException.StackTrace);
+                    }
                     return null;
                 }
                 catch (TargetInvocationException e)
                 {
-                    Console.WriteLine("QMOD ERR: Invoking the specified EntryMethod {0} failed for {1}", mod.EntryMethod, mod.Id);
-                    Console.WriteLine(e.InnerException.Message);
+                    Console.WriteLine($"ERROR! Invoking the specified entry method {mod.EntryMethod} failed for mod {mod.Id}");
+                    Console.WriteLine(e.Message);
+                    Console.WriteLine(e.StackTrace);
+                    if (e.InnerException != null)
+                    {
+                        Console.WriteLine(e.InnerException.Message);
+                        Console.WriteLine(e.InnerException.StackTrace);
+                    }
                     return null;
                 }
                 catch (Exception e)
                 {
-                    Console.WriteLine("QMOD ERR: something strange happened");
+                    Console.WriteLine("ERROR! An unexpected error occurred!");
                     Console.WriteLine(e.Message);
+                    Console.WriteLine(e.StackTrace);
+                    if (e.InnerException != null)
+                    {
+                        Console.WriteLine(e.InnerException.Message);
+                        Console.WriteLine(e.InnerException.StackTrace);
+                    }
                     return null;
                 }
             }
