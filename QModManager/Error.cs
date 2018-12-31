b4fe922989f3e11b73c6957cb3a164f3dd34b3a5
AlexejheroYTB <alexejhero@gmail.com>
2018-12-31 22:17:32 +0200

    Rename

--- QModManager/Error.cs
+++ QModManager/Error.cs
@@ -2,29 +2,35 @@
 
 namespace QModManager
 {
-    internal static class ErrorMessage
+    internal static class Error
     {
         internal static void ShowError(string error, Action<bool> function, string leftButtonText = "Yes", string rightButtonText = "No")
         {
             // Still need to implement leftButtonText and rightButtonText
             uGUI_SceneConfirmation confirmation = uGUI.main.confirmation;
 
+            if (function == null) function = (_) => { };
+
             if (string.IsNullOrEmpty(leftButtonText)) confirmation.yes.gameObject.SetActive(false);
             if (string.IsNullOrEmpty(rightButtonText)) confirmation.no.gameObject.SetActive(false);
 
             confirmation.Show(error, delegate (bool leftButtonClicked)
             {
-                if (function != null) function.Invoke(leftButtonClicked);
+                function.Invoke(leftButtonClicked);
                 confirmation.no.gameObject.SetActive(true);
                 confirmation.yes.gameObject.SetActive(true);
             });
         }
 
         internal static void ShowError(string error, Action onLeftButton, Action onRightButton, string leftButtonText = "Yes", string rightButtonText = "No")
-            => ShowError(error, leftButtonClicked =>
-            {
-                if (leftButtonClicked) if (onLeftButton != null) onLeftButton.Invoke();
-                else if (onRightButton != null) onRightButton.Invoke();
-            }, leftButtonText, rightButtonText);
+        {
+            if (onLeftButton == null) onLeftButton = () => { };
+            if (onRightButton == null) onRightButton = () => { };
+            ShowError(error, leftButtonClicked =>
+                       {
+                           if (leftButtonClicked) onLeftButton.Invoke();
+                               else onRightButton.Invoke();
+                       }, leftButtonText, rightButtonText);
+        }
     }
 }
