dee738ff9ee2fd4af59bd6896587ab6e7c84b0a1
AlexejheroYTB <alexejhero@gmail.com>
2018-12-31 20:45:38 +0200

    Add code

---
+++ QModManager/ErrorMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace QModManager
+{
+    public static class ErrorMessage
+    {
+        public static void ShowError(string error, Action<bool> function, string leftButtonText = "Yes", bool disableRightButton = false, string RightButtonText = "No")
+        {
+            uGUI_SceneConfirmation confirmation = uGUI.main.confirmation;
+            confirmation.no.gameObject.SetActive(true);
+            confirmation.Show(error, delegate (bool confirmed) 
+            {
+                function.Invoke(confirmed);
+                if (disableRightButton) confirmation.no.gameObject.SetActive(true);
+            });
+        }
+    }
+}
