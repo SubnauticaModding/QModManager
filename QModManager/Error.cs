46f79a557f1f87bea714659da2fd55cdd30c2176
AlexejheroYTB <alexejhero@gmail.com>
2019-01-01 15:19:14 +0200

    Update Error.cs

--- QModManager/Error.cs
+++ QModManager/Error.cs
@@ -1,36 +1,37 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
+using UnityEngine.UI;
 
 namespace QModManager
 {
     internal static class Error
     {
-        internal static void ShowError(string error, Action<bool> function, string leftButtonText = "Yes", string rightButtonText = "No")
+        internal static void ShowError(string error, Action onLeftButtonPressed = null, Action onRightButtonPressed = null, string leftButtonText = "See Log", string rightButtonText = "Close")
         {
-            // Still need to implement leftButtonText and rightButtonText
             uGUI_SceneConfirmation confirmation = uGUI.main.confirmation;
 
-            if (function == null) function = (_) => { };
+            if (onLeftButtonPressed == null) onLeftButtonPressed = () => { Process.Start(Path.Combine(QModPatcher.QModBaseDir, "../Subnautica_Data/output_log.txt")); };
+            if (onRightButtonPressed == null) onRightButtonPressed = () => { Console.Write(""); };
 
             if (string.IsNullOrEmpty(leftButtonText)) confirmation.yes.gameObject.SetActive(false);
+            else confirmation.yes.gameObject.GetComponentInChildren<Text>().text = leftButtonText;
             if (string.IsNullOrEmpty(rightButtonText)) confirmation.no.gameObject.SetActive(false);
+            else confirmation.no.gameObject.GetComponentInChildren<Text>().text = rightButtonText;
 
             confirmation.Show(error, delegate (bool leftButtonClicked)
             {
-                function.Invoke(leftButtonClicked);
-                confirmation.no.gameObject.SetActive(true);
-                confirmation.yes.gameObject.SetActive(true);
-            });
-        }
+                if (leftButtonClicked) onLeftButtonPressed.Invoke();
+                else onRightButtonPressed.Invoke();
 
-        internal static void ShowError(string error, Action onLeftButton, Action onRightButton, string leftButtonText = "Yes", string rightButtonText = "No")
-        {
-            if (onLeftButton == null) onLeftButton = () => { };
-            if (onRightButton == null) onRightButton = () => { };
-            ShowError(error, leftButtonClicked =>
-                       {
-                           if (leftButtonClicked) onLeftButton.Invoke();
-                               else onRightButton.Invoke();
-                       }, leftButtonText, rightButtonText);
+                Console.Write("");
+
+                confirmation.yes.gameObject.SetActive(true);
+                confirmation.no.gameObject.SetActive(true);
+
+                confirmation.yes.gameObject.GetComponentInChildren<Text>().text = "Yes";
+                confirmation.no.gameObject.GetComponentInChildren<Text>().text = "No";
+            });
         }
     }
 }
