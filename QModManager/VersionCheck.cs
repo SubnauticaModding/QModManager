174096a9555117aba95e757dbbdd55762161cf97
AlexejheroYTB <alexejhero@gmail.com>
2019-01-03 12:41:50 +0200

    Add GetAsync method

---
+++ QModManager/VersionCheck.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace QModManager
+{
+    internal static class VersionCheck
+    {
+        public static async Task<string> GetAsync(string uri)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+            using (HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return await reader.ReadToEndAsync();
+            }
+        }
+    }
+}
