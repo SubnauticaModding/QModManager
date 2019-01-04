192c37fb4f18947750e8c403323709deafd41240
AlexejheroYTB <alexejhero@gmail.com>
2019-01-04 09:19:42 +0200

    Update project

--- Unity App/Assets/Scripts/DirectorySelector.cs
+++ Unity App/Assets/Scripts/DirectorySelector.cs
@@ -6,68 +6,71 @@ using UnityEngine.UI;
 
 public class DirectorySelector : MonoBehaviour
 {
-    private const string FolderPref = "SubnauticaInstallDirectory";
+    public static DirectorySelector singleton;
 
-    [SerializeField]
-    private TextMeshProUGUI _label;
-    [SerializeField]
-    private TextMeshProUGUI _statusLabel;
-    [SerializeField]
-    private Image _icon;
-    [SerializeField]
-    private Sprite _noFolderIcon;
-    [SerializeField]
-    private Sprite _hasFolderIcon;
+    [ReadOnly]
+    public string FolderPref = "SubnauticaInstallDirectory";
 
-    private void Awake()
+    public TextMeshProUGUI label;
+    public TextMeshProUGUI statusLabel;
+    public Image icon;
+    public Sprite noFolderIcon;
+    public Sprite hasFolderIcon;
+    public Color green;
+    public Color red;
+
+    public void Awake()
     {
+        if (singleton != null)
+        {
+            Debug.LogError("DirectorySelector should be singleton but it isn't!");
+            Destroy(this);
+        }
+        singleton = this;
         Refresh();
     }
 
-    private string GetInstallFolderPref()
+    public string GetInstallFolderPref()
     {
         return PlayerPrefs.GetString(FolderPref, string.Empty);
     }
 
-    private void Refresh()
+    public void Refresh()
     {
-        var currentSelectedInstallFolder = GetInstallFolderPref();
+        string currentSelectedInstallFolder = GetInstallFolderPref();
         if (InstallFolderIsValid(currentSelectedInstallFolder))
         {
-            _label.text = currentSelectedInstallFolder;
-            _icon.overrideSprite = _hasFolderIcon;
+            label.text = currentSelectedInstallFolder;
+            icon.overrideSprite = hasFolderIcon;
         }
         else
         {
-            _label.text = "Select Subnautica Install Directory...";
-            _icon.overrideSprite = _noFolderIcon;
+            label.text = "Select Subnautica Install Directory...";
+            icon.overrideSprite = noFolderIcon;
         }
 
         RefreshStatusLabel(currentSelectedInstallFolder);
     }
 
-    private void RefreshStatusLabel(string installFolder)
+    public void RefreshStatusLabel(string installFolder)
     {
         if (string.IsNullOrEmpty(installFolder))
         {
-            _statusLabel.text = "";
+            statusLabel.text = "";
             return;
         }
 
-        var isSubnauticaInstalled = CheckSubnauticaInstalled(installFolder);
-        var isQModsPatched = CheckQModsPatched(installFolder);
+        bool isSubnauticaInstalled = CheckSubnauticaInstalled(installFolder);
+        bool isQModsPatched = CheckQModsPatched(installFolder);
 
-        const string green = "#54C069";
-        const string red = "#EA4F46";
+        Color snInstalledColor = isSubnauticaInstalled ? green : red;
+        string snInstalledText = isSubnauticaInstalled ? "INSTALLED" : "NOT INSTALLED";
 
-        var snInstalledColor = isSubnauticaInstalled ? green : red;
-        var snInstalledText = isSubnauticaInstalled ? "INSTALLED" : "NOT INSTALLED";
-
-        _statusLabel.text = $"Subnautica <color={snInstalledColor}>{snInstalledText}</color> ";
+        statusLabel.text = $"Subnautica <color={snInstalledColor}>{snInstalledText}</color> ";
 
         var qmodsPatchedColor = isQModsPatched ? green : red;
         var qmodsPatchedText = isQModsPatched ? "PATCHED" : "NOT PATCHED";
-        _statusLabel.text += $" - QMods <color={qmodsPatchedColor}>{qmodsPatchedText} (TODO)</color> ";
+        statusLabel.text += $" - QMods <color={qmodsPatchedColor}>{qmodsPatchedText} (TODO)</color> ";
     }
 
     private static bool CheckSubnauticaInstalled(string installFolder)
---
+++ Unity App/Assets/Scripts/Util/Editor/ReadOnlyPropertyDrawer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+public class ReadOnlyPropertyDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        GUI.enabled = false;
+        EditorGUI.PropertyField(position, property, label);
+        GUI.enabled = true;
+    }
+}
