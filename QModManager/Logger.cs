10e226277456b5aee2cb68b95e778fc3151905bf
AlexejheroYTB <alexejhero@gmail.com>
2019-01-06 12:32:59 +0200

    Create Logger.cs

---
+++ QModManager/Logger.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+
+namespace QModManager
+{
+    public static class Logger
+    {
+        public static void ShowSubtitle(string text, float delay = 1f, float duration = 5f)
+        {
+            typeof(Subtitles).GetMethod("AddRawLong", BindingFlags.NonPublic | BindingFlags.Instance)?.Invoke(Subtitles.main, new object[] { text, delay, duration });
+        }
+    }
+}
+
