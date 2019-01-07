c90fcfc9fc397c7b61b154f946cf313e52963303
AlexejheroYTB <alexejhero@gmail.com>
2019-01-07 17:48:21 +0200

    Remove DCF

---
+++ Unity App/Assets/Scripts/Admin.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class Admin : MonoBehaviour
+{
+    public static Admin singleton;
+
+    public void Awake()
+    {
+        if (singleton != null)
+        {
+            Debug.LogError("Admin is supposed to be a singleton but isn't!");
+            Destroy(gameObject);
+        }
+        singleton = this;
+    }
+
+    public void Start()
+    {
+        if (!Application.isEditor)
+        {
+            Destroy();
+            return;
+        }
+    }
+
+    public void Destroy()
+    {
+        Destroy(GetComponent<SidebarMenuItem>().content);
+        Destroy(gameObject);
+    }
+}
