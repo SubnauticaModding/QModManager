161ef9e28e4fd212a77db35823b08e1400b6c654
AlexejheroYTB <alexejhero@gmail.com>
2019-01-16 20:08:25 +0200

    Update PatchUtils.cs

--- SMLHelper/Utility/PatchUtils.cs
+++ SMLHelper/Utility/PatchUtils.cs
@@ -29,8 +29,8 @@
 
         public static void PatchList(Type type, string name, IList list, BindingFlags flags)
         {
-            var craftDataList = listField.GetValue(null) as IList;
             FieldInfo listField = type.GetField(name, flags);
+            var craftDataList = listField.GetValue(null) as IList;
 
             foreach (object obj in list)
             {
