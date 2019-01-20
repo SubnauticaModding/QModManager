cec51f1951c340c4ebc4958e7f068e5e2b0475be
AlexejheroYTB <alexejhero@gmail.com>
2019-01-20 13:19:12 +0200

    Rename and add docs for PlayerPrefsExtra

---
+++ SMLHelper/Utility/PlayerPrefsExtra.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace SMLHelper.V2.Utility
+{
+    public static class PlayerPrefsExtra
+    {
+        /// <summary>
+        /// Get a boolean value using PlayerPrefs
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="defaultValue"></param>
+        /// <returns></returns>
+        public static bool GetBool(string key, bool defaultValue)
+        {
+            return PlayerPrefs.GetInt(key, defaultValue == true ? 1 : 0) == 1 ? true : false;
+        }
+
+        /// <summary>
+        /// Set a boolean value using PlayerPrefs
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        public static void SetBool(string key, bool value)
+        {
+            PlayerPrefs.SetInt(key, value == true ? 1 : 0);
+        }
+    }
+}
