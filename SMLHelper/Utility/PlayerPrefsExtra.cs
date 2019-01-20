711b724495c7486ff6532b80607e28a9d1ac301c
AlexejheroYTB <alexejhero@gmail.com>
2019-01-20 15:45:55 +0200

    PlayerPrefsExtra: Vector2

--- SMLHelper/Utility/PlayerPrefsExtra.cs
+++ SMLHelper/Utility/PlayerPrefsExtra.cs
@@ -43,5 +43,38 @@ namespace SMLHelper.V2.Utility
         {
             PlayerPrefs.SetString(key, KeyCodeUtils.KeyCodeToString(value));
         }
+
+        /// <summary>
+        /// Get a <see cref="Vector2"/> value using <see cref="PlayerPrefs"/>
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static Vector2 GetVector2(string key)
+        {
+            return GetVector2(key, Vector2.zero);
+        }
+        /// <summary>
+        /// Get a <see cref="Vector2"/> value using <see cref="PlayerPrefs"/>
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="defaultValue"></param>
+        /// <returns></returns>
+        public static Vector2 GetVector2(string key, Vector2 defaultValue)
+        {
+            float x = PlayerPrefs.GetFloat($"{key}_vector2_x", defaultValue.x);
+            float y = PlayerPrefs.GetFloat($"{key}_vector2_y", defaultValue.y);
+
+            return new Vector2(x, y);
+        }
+        /// <summary>
+        /// Set a <see cref="Vector2"/> value using <see cref="PlayerPrefs"/>
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        public static void SetVector2(string key, Vector2 value)
+        {
+            PlayerPrefs.SetFloat($"{key}_vector2_x", value.x);
+            PlayerPrefs.SetFloat($"{key}_vector2_y", value.y);
+        }
     }
 }
