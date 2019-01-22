ad05e0d81081504d05ae69303fd3ba53ca0a49ce
AlexejheroYTB <alexejhero@gmail.com>
2019-01-22 20:21:19 +0200

    Remove code that's not called and fix stuff

--- QModManager/Injector.cs
+++ QModManager/Injector.cs
@@ -13,17 +13,10 @@ namespace QModManager
         internal string installerFilename = "QModInstaller.dll";
         internal string mainFilename = "Assembly-CSharp.dll";
 
-        internal QModInjector(string dir, string managedDir = null)
+        internal QModInjector(string dir, string managedDir)
         {
             gameDirectory = dir;
-			if (managedDir == null)
-			{
-				managedDirectory = Path.Combine(gameDirectory, @"Subnautica_Data\Managed");
-			}
-			else
-			{
-				managedDirectory = managedDir;
-			}
+			managedDirectory = managedDir;
             mainFilename = Path.Combine(managedDirectory, mainFilename);
         }
 
