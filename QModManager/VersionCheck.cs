b21da36e9d2a67c63b7738621714a2ed8b3a8c61
AlexejheroYTB <alexejhero@gmail.com>
2019-01-31 19:35:38 +0200

    Reorganize versioncheck.cs

--- QModManager/VersionCheck.cs
+++ QModManager/VersionCheck.cs
@@ -1,6 +1,5 @@
 ﻿using Harmony;
 using System;
-using System.Diagnostics;
 using System.Net;
 using System.Net.Security;
 using System.Security.Cryptography.X509Certificates;
@@ -12,26 +11,6 @@ namespace QModManager
     internal static class VersionCheck
     {
         internal const string nexusmodsURL = "https://nexusmods.com/subnautica/mods/16";
-
-        internal static void Parse(string versionStr)
-        {
-            if (versionStr == null)
-            {
-                UnityEngine.Debug.Log("Could not get latest version!");
-                return;
-            }
-            Version version = new Version(versionStr);
-            if (version == null)
-            {
-                UnityEngine.Debug.Log("Could not get latest version!");
-                return;
-            }
-            if (!version.Equals(QMod.QModManagerVersion) && QModPatcher.erroredMods.Count <= 0)
-                Dialog.Show($"There is a newer version of QModManager available: {version.ToString()} " + 
-                    "(current version: {QMod.QModManagerVersion.ToString()})", 
-                () => Process.Start(nexusmodsURL), leftButtonText: "Download", blue: true);
-        }
-
         internal const string VersionURL = "https://raw.githubusercontent.com/QModManager/QModManager/master/latest-version.txt";
 
         private static float timer = 0f;
@@ -59,12 +38,29 @@ namespace QModManager
                 };
             }
         }
+        private static void Parse(string versionStr)
+        {
+            if (versionStr == null)
+            {
+                UnityEngine.Debug.Log("Could not get latest version!");
+                return;
+            }
+            Version version = new Version(versionStr);
+            if (version == null)
+            {
+                UnityEngine.Debug.Log("Could not get latest version!");
+                return;
+            }
+            if (!version.Equals(QMod.QModManagerVersion) && QModPatcher.erroredMods.Count <= 0)
+                Dialog.Show($"There is a newer version of QModManager available: {version.ToString()} " +
+                    "(current version: {QMod.QModManagerVersion.ToString()})", Dialog.Button.download, Dialog.Button.close, true);
+        }
 
         [HarmonyPatch(typeof(uGUI_OptionsPanel), "AddTabs")]
-        internal static class AddTabPatch
+        private static class AddTabPatch
         {
             [HarmonyPostfix]
-            internal static void Postfix(uGUI_OptionsPanel __instance)
+            private static void Postfix(uGUI_OptionsPanel __instance)
             {
                 bool currentValue = PlayerPrefs.GetInt("QModManager_EnableUpdateCheck", 1) == 0 ? false : true;
                 int modsTab = __instance.AddTab("Mods");
@@ -74,8 +70,7 @@ namespace QModManager
             }
         }
 
-        private static bool CustomRemoteCertificateValidationCallback(object sender,
-    X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+        private static bool CustomRemoteCertificateValidationCallback(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
         {
             bool isOk = true;
             // If there are errors in the certificate chain,
