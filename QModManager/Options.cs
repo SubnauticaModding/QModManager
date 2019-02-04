75889e5362cf4ea51ebe62961a05f79330ea2688
AlexejheroYTB <alexejhero@gmail.com>
2019-02-04 17:33:29 +0200

    Debugger on by default

--- QModManager/Options.cs
+++ QModManager/Options.cs
@@ -13,7 +13,7 @@ namespace QModManager
             private static void Postfix(uGUI_OptionsPanel __instance)
             {
                 bool updateCheck = PlayerPrefs.GetInt("QModManager_EnableUpdateCheck", 1) == 0 ? false : true;
-                bool enableDebugger = PlayerPrefs.GetInt("QModManager_PrefabDebugger_Enable", 0) == 0 ? false : true;
+                bool enableDebugger = PlayerPrefs.GetInt("QModManager_PrefabDebugger_Enable", 1) == 0 ? false : true;
                 int modsTab = __instance.AddTab("Mods");
                 __instance.AddHeading(modsTab, "QModManager");
                 __instance.AddToggleOption(modsTab, "Check for updates", updateCheck,
