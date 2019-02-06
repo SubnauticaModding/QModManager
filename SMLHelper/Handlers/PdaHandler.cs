cd5dc3ff0d6616b6300ef2c025c882762f8b7b42
PrimeSonic <prime.sonic@gmail.com>
2019-02-06 06:46:03 -0500

    PdaScanPatching - New Handler and Patcher - Can now edit number of required fragments to be scanned for a blueprint unlock - Can also edit the time taken to scan a fragment - Reverse lookups handled so the modder can use either the fragment techtype or the blueprint techtype

---
+++ SMLHelper/Handlers/PdaHandler.cs
@@ -0,0 +1,41 @@
+﻿namespace SMLHelper.V2.Handlers
+{
+    using SMLHelper.V2.Patchers;
+
+    /// <summary>
+    /// A handler class for various scanner related data.
+    /// </summary>
+    public static class PdaHandler
+    {
+
+        /// <summary>
+        /// Edits how many fragments must be scanned before unlocking the techtype's blueprint.
+        /// </summary>
+        /// <param name="techType">Can be either techtype of the fragment or the crafted item.</param>
+        /// <param name="fragmentCount">The number of fragments to scan.</param>
+        public static void EditFragmentsToScan(TechType techType, int fragmentCount)
+        {
+            if (fragmentCount <= 0)
+            {
+                fragmentCount = 1;
+            }
+
+            PdaPatcher.FragmentCount[techType] = fragmentCount;
+        }
+
+        /// <summary>
+        /// Edits the time it takes to finish scanning a fragment.
+        /// </summary>
+        /// <param name="techType">Can be either techtype of the fragment or the crafted item.</param>
+        /// <param name="scanTime">The relative time spent on scanning. Default value is 1.</param>
+        public static void EditFragmentScanTime(TechType techType, float scanTime)
+        {
+            if (scanTime <= 0f)
+            {
+                scanTime = 1f;
+            }
+
+            PdaPatcher.FragmentScanTime[techType] = scanTime;
+        }
+    }
+}
---
+++ SMLHelper/Patchers/PdaPatcher.cs
@@ -0,0 +1,88 @@
+﻿namespace SMLHelper.V2.Patchers
+{
+    using System.Collections.Generic;
+    using System.Reflection;
+    using Harmony;
+
+    // Special thanks to Gorillazilla9 for sharing this method of fragment count patching
+    // https://github.com/Gorillazilla9/SubnauticaFragReqBoost/blob/master/PDAScannerPatcher.cs
+    internal class PdaPatcher
+    {
+        internal static Dictionary<TechType, int> FragmentCount = new Dictionary<TechType, int>();
+        internal static Dictionary<TechType, float> FragmentScanTime = new Dictionary<TechType, float>();
+
+        private static Dictionary<TechType, PDAScanner.EntryData> BlueprintToFragment = new Dictionary<TechType, PDAScanner.EntryData>();
+
+        internal static void Patch(HarmonyInstance harmony)
+        {
+            MethodInfo initializeInfo = typeof(PDAScanner).GetMethod("Initialize", BindingFlags.Public | BindingFlags.Static);
+            MethodInfo postfixInfo = typeof(PdaPatcher).GetMethod("InitializePostfix", BindingFlags.NonPublic | BindingFlags.Static);
+
+            harmony.Patch(initializeInfo, null, new HarmonyMethod(postfixInfo));
+        }
+
+        private static void InitializePostfix()
+        {
+            PopulateReverseLookup();
+
+            UpdateFragmentTotals();
+
+            UpdateScanTimes();
+        }
+
+        private static void UpdateFragmentTotals()
+        {
+            foreach (KeyValuePair<TechType, int> fragmentEntry in FragmentCount)
+            {
+                if (PDAScanner.HasEntry(fragmentEntry.Key)) // Lookup by techtype of fragment
+                {
+                    PDAScanner.GetEntryData(fragmentEntry.Key).totalFragments = fragmentEntry.Value;
+                }
+                else if (BlueprintToFragment.TryGetValue(fragmentEntry.Key, out PDAScanner.EntryData entryData)) // Lookup by blueprint techtype
+                {
+                    entryData.totalFragments = fragmentEntry.Value;
+                }
+                else
+                {
+                    Logger.Log($"Warning: TechType {fragmentEntry.Key} not known in PDAScanner.EntryData");
+                }
+            }
+        }
+
+        private static void UpdateScanTimes()
+        {
+            foreach (KeyValuePair<TechType, float> fragmentEntry in FragmentScanTime)
+            {
+                if (PDAScanner.HasEntry(fragmentEntry.Key)) // Lookup by techtype of fragment
+                {
+                    PDAScanner.GetEntryData(fragmentEntry.Key).scanTime = fragmentEntry.Value;
+                }
+                else if (BlueprintToFragment.TryGetValue(fragmentEntry.Key, out PDAScanner.EntryData entryData)) // Lookup by blueprint techtype
+                {
+                    entryData.scanTime = fragmentEntry.Value;
+                }
+                else
+                {
+                    Logger.Log($"Warning: TechType {fragmentEntry.Key} not known in PDAScanner.EntryData");
+                }
+            }
+        }
+
+        private static void PopulateReverseLookup()
+        {
+            BlueprintToFragment.Clear();
+
+            Dictionary<TechType, PDAScanner.EntryData>.Enumerator entries = PDAScanner.GetAllEntriesData();
+
+            do
+            {
+                KeyValuePair<TechType, PDAScanner.EntryData> entry = entries.Current;
+
+                TechType blueprintTechType = entry.Value.blueprint;
+
+                BlueprintToFragment[blueprintTechType] = entry.Value;
+
+            } while (entries.MoveNext());
+        }
+    }
+}
