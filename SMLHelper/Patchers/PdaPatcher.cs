c5a79e6a8f242252fffd702312b71f4d83141f60
PrimeSonic <prime.sonic@gmail.com>
2019-02-10 10:26:38 -0500

    PdaScanPatching - Fixes the bug that caused all craft data and known tech to break

--- SMLHelper/Patchers/PdaPatcher.cs
+++ SMLHelper/Patchers/PdaPatcher.cs
@@ -1,5 +1,6 @@
 ﻿namespace SMLHelper.V2.Patchers
 {
+    using System;
     using System.Collections.Generic;
     using System.Reflection;
     using Harmony;
@@ -11,32 +12,41 @@
         internal static Dictionary<TechType, int> FragmentCount = new Dictionary<TechType, int>();
         internal static Dictionary<TechType, float> FragmentScanTime = new Dictionary<TechType, float>();
 
+        private static Type pdaScannerType = typeof(PDAScanner);
         private static Dictionary<TechType, PDAScanner.EntryData> BlueprintToFragment = new Dictionary<TechType, PDAScanner.EntryData>();
 
         internal static void Patch(HarmonyInstance harmony)
         {
-            MethodInfo initializeInfo = typeof(PDAScanner).GetMethod("Initialize", BindingFlags.Public | BindingFlags.Static);
+            MethodInfo initializeInfo = pdaScannerType.GetMethod("Initialize", BindingFlags.Public | BindingFlags.Static);
             MethodInfo postfixInfo = typeof(PDAPatcher).GetMethod("InitializePostfix", BindingFlags.NonPublic | BindingFlags.Static);
 
             harmony.Patch(initializeInfo, null, new HarmonyMethod(postfixInfo));
+
+            Logger.Log($"PDAPatcher is ready.");
         }
 
         private static void InitializePostfix()
         {
-            PopulateReverseLookup();
+            BlueprintToFragment.Clear();
 
-            UpdateFragmentTotals();
+            var pdaTraverse = Traverse.Create(pdaScannerType);
+            Traverse pdaMappingTraverse = pdaTraverse.Field("mapping");
+            Dictionary<TechType, PDAScanner.EntryData> mapping = pdaMappingTraverse.GetValue<Dictionary<TechType, PDAScanner.EntryData>>();
 
-            UpdateScanTimes();
-        }
+            // Populate BlueprintToFragment for reverse lookup
+            foreach (KeyValuePair<TechType, PDAScanner.EntryData> entry in mapping)
+            {
+                TechType blueprintTechType = entry.Value.blueprint;
 
-        private static void UpdateFragmentTotals()
-        {
+                BlueprintToFragment[blueprintTechType] = entry.Value;
+            }
+
+            // Update fragment totals
             foreach (KeyValuePair<TechType, int> fragmentEntry in FragmentCount)
             {
-                if (PDAScanner.HasEntry(fragmentEntry.Key)) // Lookup by techtype of fragment
+                if (mapping.ContainsKey(fragmentEntry.Key)) // Lookup by techtype of fragment
                 {
-                    PDAScanner.GetEntryData(fragmentEntry.Key).totalFragments = fragmentEntry.Value;
+                    mapping[fragmentEntry.Key].totalFragments = fragmentEntry.Value;
                 }
                 else if (BlueprintToFragment.TryGetValue(fragmentEntry.Key, out PDAScanner.EntryData entryData)) // Lookup by blueprint techtype
                 {
@@ -47,15 +57,13 @@
                     Logger.Log($"Warning: TechType {fragmentEntry.Key} not known in PDAScanner.EntryData");
                 }
             }
-        }
 
-        private static void UpdateScanTimes()
-        {
+            // Update scan times
             foreach (KeyValuePair<TechType, float> fragmentEntry in FragmentScanTime)
             {
-                if (PDAScanner.HasEntry(fragmentEntry.Key)) // Lookup by techtype of fragment
+                if (mapping.ContainsKey(fragmentEntry.Key)) // Lookup by techtype of fragment
                 {
-                    PDAScanner.GetEntryData(fragmentEntry.Key).scanTime = fragmentEntry.Value;
+                    mapping[fragmentEntry.Key].scanTime = fragmentEntry.Value;
                 }
                 else if (BlueprintToFragment.TryGetValue(fragmentEntry.Key, out PDAScanner.EntryData entryData)) // Lookup by blueprint techtype
                 {
@@ -66,23 +74,8 @@
                     Logger.Log($"Warning: TechType {fragmentEntry.Key} not known in PDAScanner.EntryData");
                 }
             }
-        }
 
-        private static void PopulateReverseLookup()
-        {
-            BlueprintToFragment.Clear();
-
-            Dictionary<TechType, PDAScanner.EntryData>.Enumerator entries = PDAScanner.GetAllEntriesData();
-
-            do
-            {
-                KeyValuePair<TechType, PDAScanner.EntryData> entry = entries.Current;
-
-                TechType blueprintTechType = entry.Value.blueprint;
-
-                BlueprintToFragment[blueprintTechType] = entry.Value;
-
-            } while (entries.MoveNext());
+            Logger.Log($"PDAPatcher is done.");
         }
     }
 }
