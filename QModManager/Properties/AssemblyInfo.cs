587eec29fe1e53fdf60c41c38682ddc3401760a8
AlexejheroYTB <alexejhero@gmail.com>
2019-02-19 20:20:16 +0200

    Remove guid from QMM AssemblyInfo since the project is com invisible

--- QModManager/Properties/AssemblyInfo.cs
+++ QModManager/Properties/AssemblyInfo.cs
@@ -13,8 +13,6 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("e00b7fe8-0f1d-4ae6-9e47-4bfd81537f14")]
-
 [assembly: AssemblyVersion("2.0.1.0")]
 [assembly: AssemblyFileVersion("2.0.1.0")]
 
