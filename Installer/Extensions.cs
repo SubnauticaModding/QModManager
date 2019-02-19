b79c8bc433592763f4c73312ef4ceb752f5b3784
AlexejheroYTB <alexejhero@gmail.com>
2019-02-19 20:20:00 +0200

    Create InstallerExtensions project

---
+++ Installer/Extensions.cs
@@ -0,0 +1,18 @@
+﻿using RGiesecke.DllExport;
+using System;
+using System.IO;
+
+namespace QModManager.Installer.Extensions
+{
+    public static class Extensions
+    {
+        [DllExport("PathsEqual", System.Runtime.InteropServices.CallingConvention.StdCall)]
+        public static bool PathsEqual(string path1, string path2)
+        {
+            string path1parsed = Path.GetFullPath(path1);
+            string path2parsed = Path.GetFullPath(path2);
+
+            return string.Equals(path1parsed, path2parsed, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
---
+++ Installer/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("QModManagerInstallerExtensions")]
+[assembly: AssemblyDescription("Config based patch management for Subnautica and Subnautica: Below Zero")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("the QModManager dev team")]
+[assembly: AssemblyProduct("QModManagerInstallerExtensions")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("2.0.1.0")]
+[assembly: AssemblyFileVersion("2.0.1.0")]
