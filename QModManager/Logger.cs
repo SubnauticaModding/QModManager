a7c410f0f2333615e7bc7e5ce4a3c1d0895e7a66
AlexejheroYTB <alexejhero@gmail.com>
2019-02-23 16:34:24 +0200

    Add Log method with no prefix

--- QModManager/Logger.cs
+++ QModManager/Logger.cs
@@ -4,11 +4,12 @@ namespace QModManager
 {
     internal static class Logger
     {
-        private static void Log(string prefix, string text) => Console.WriteLine($"[QModManager/{prefix}] {text}");
-        internal static void Debug(string text) => Log("DEBUG", text);
-        internal static void Info(string text) => Log("INFO", text);
-        internal static void Warn(string text) => Log("WARN", text);
-        internal static void Error(string text) => Log("ERROR", text);
-        internal static void Fatal(string text) => Log("FATAL", text);
+        private static void Log(string prefix, string text) => Console.WriteLine($"[QModManager{prefix}] {text}");
+        internal static void Log(string text) => Log("", text);
+        internal static void Debug(string text) => Log("/DEBUG", text);
+        internal static void Info(string text) => Log("/INFO", text);
+        internal static void Warn(string text) => Log("/WARN", text);
+        internal static void Error(string text) => Log("/ERROR", text);
+        internal static void Fatal(string text) => Log("/FATAL", text);
     }
 }
