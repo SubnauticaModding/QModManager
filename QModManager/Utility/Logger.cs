8755193eaace50fd23a149b30c7963b359afeebb
AlexejheroYTB <alexejhero@gmail.com>
2019-02-23 17:10:42 +0200

    Update logger to use "Enable debug logs"

--- QModManager/Utility/Logger.cs
+++ QModManager/Utility/Logger.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 
-namespace QModManager
+namespace QModManager.Utility
 {
     internal static class Logger
     {
+        private static bool EnableDebugging
+        {
+            get
+            {
+                return PlayerPrefsExtra.GetBool("QModManager_EnableDebugLogs", false);
+            }
+            set
+            {
+                PlayerPrefsExtra.SetBool("QModManager_EnableDebugLogs", value);
+            }
+        }
+
         private static void Log(string prefix, string text) => Console.WriteLine($"[QModManager{prefix}] {text}");
         internal static void Log(string text) => Log("", text);
-        internal static void Debug(string text) => Log("/DEBUG", text);
+        internal static void Debug(string text) { if (EnableDebugging) Log("/DEBUG", text); }
         internal static void Info(string text) => Log("/INFO", text);
         internal static void Warn(string text) => Log("/WARN", text);
         internal static void Error(string text) => Log("/ERROR", text);
