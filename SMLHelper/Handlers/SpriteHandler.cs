b4b377754965705e0ac7d2b53b6bc8078c50fa90
PrimeSonic <prime.sonic@gmail.com>
2019-03-13 20:27:19 -0400

    InterfacesOverhaul - Added interface for LanguageHandler - Added missing XML documentation to SpriteHandler

--- SMLHelper/Handlers/SpriteHandler.cs
+++ SMLHelper/Handlers/SpriteHandler.cs
@@ -17,45 +17,105 @@
             // Hide constructor
         }
 
+        #region Static Methods
+
+        /// <summary>
+        /// Registers a new sprite to the game.
+        /// </summary>
+        /// <param name="type">The techtype paired to this sprite.</param>
+        /// <param name="sprite">The sprite to be added.</param>
         public static void RegisterSprite(TechType type, Atlas.Sprite sprite)
         {
             Main.RegisterSprite(type, sprite);
         }
 
+        /// <summary>
+        /// Registers a new sprite to the game.
+        /// </summary>
+        /// <param name="group">The sprite group.</param>
+        /// <param name="id">The sprite internal identifier.</param>
+        /// <param name="sprite">The sprite to be added.</param>
         public static void RegisterSprite(SpriteManager.Group group, string id, Atlas.Sprite sprite)
         {
             Main.RegisterSprite(group, id, sprite);
         }
 
+        /// <summary>
+        /// Registers a new sprite to the game.
+        /// </summary>
+        /// <param name="group">The sprite group this sprite will be added to.</param>
+        /// <param name="id">The sprite internal identifier.</param>
+        /// <param name="sprite">The sprite to be added.</param>
         public static void RegisterSprite(SpriteManager.Group group, string id, Sprite sprite)
         {
             Main.RegisterSprite(group, id, sprite);
         }
 
+        /// <summary>
+        /// Registers a new sprite to the game.
+        /// </summary>
+        /// <param name="type">The techtype paired to this sprite.</param>
+        /// <param name="sprite">The sprite to be added.</param>
         public static void RegisterSprite(TechType type, Sprite sprite)
         {
             Main.RegisterSprite(type, sprite);
         }
 
+        /// <summary>
+        /// Registers a new sprite to the game.
+        /// </summary>
+        /// <param name="type">The techtype paired to this sprite.</param>
+        /// <param name="filePathToImage">The file path to image to be converted into a sprite.</param>
+        /// <seealso cref="ImageUtils.LoadSpriteFromFile(string, TextureFormat)" />
         public static void RegisterSprite(TechType type, string filePathToImage)
         {
             Main.RegisterSprite(type, filePathToImage);
         }
 
+        /// <summary>
+        /// Registers a new sprite to the game.
+        /// </summary>
+        /// <param name="type">The techtype paired to this sprite.</param>
+        /// <param name="filePathToImage">The file path to image to be converted into a sprite.</param>
+        /// <param name="format"><para>The texture format. By default, this uses <see cref="TextureFormat.BC7" />.</para>
+        /// <para>https://docs.unity3d.com/ScriptReference/TextureFormat.BC7.html</para>
+        /// <para>Don't change this unless you really know what you're doing.</para></param>
+        /// <seealso cref="ImageUtils.LoadSpriteFromFile(string, TextureFormat)" />
         public static void RegisterSprite(TechType type, string filePathToImage, TextureFormat format)
         {
             Main.RegisterSprite(type, filePathToImage, format);
         }
 
+        /// <summary>
+        /// Registers a new sprite to the game.
+        /// </summary>
+        /// <param name="group">The sprite group.</param>
+        /// <param name="id">The sprite internal identifier.</param>
+        /// <param name="filePathToImage">The file path to image.</param>
+        /// <seealso cref="ImageUtils.LoadSpriteFromFile(string, TextureFormat)" />
         public static void RegisterSprite(SpriteManager.Group group, string id, string filePathToImage)
         {
             Main.RegisterSprite(group, id, filePathToImage);
         }
 
+        /// <summary>
+        /// Registers a new sprite to the game.
+        /// </summary>
+        /// <param name="group">The sprite group.</param>
+        /// <param name="id">The sprite internal identifier.</param>
+        /// <param name="filePathToImage">The file path to image.</param>
+        /// <param name="format"><para>The texture format. By default, this uses <see cref="TextureFormat.BC7" />.</para>
+        /// <para>https://docs.unity3d.com/ScriptReference/TextureFormat.BC7.html</para>
+        /// <para>Don't change this unless you really know what you're doing.</para></param>
+        /// <seealso cref="ImageUtils.LoadSpriteFromFile(string, TextureFormat)" />
         public static void RegisterSprite(SpriteManager.Group group, string id, string filePathToImage, TextureFormat format)
         {
             Main.RegisterSprite(group, id, filePathToImage, format);
         }
+
+        #endregion
+
+        #region Interface Methods
 
         /// <summary>
         /// Registers a new sprite to the game.
@@ -158,5 +218,7 @@
 
             Main.RegisterSprite(group, id, sprite);
         }
+
+        #endregion
     }
 }
