e22a8cd9867688b0b6c9c78b63a17d6161a9f16c
kylinator25 <kmcg02@outlook.com>
2019-04-09 10:16:12 +1000

    Signed-off-by: kylinator25 <kmcg02@outlook.com>

---
+++ SMLHelper/Interfaces/ILanguageHandler.cs
@@ -0,0 +1,26 @@
+﻿namespace SMLHelper.V2.Interfaces
+{
+    public interface ILanguageHandler
+    {
+        /// <summary>
+        /// Allows you to define a language entry into the game.
+        /// </summary>
+        /// <param name="lineId">The ID of the entry, this is what is used to get the actual text.</param>
+        /// <param name="text">The actual text related to the entry.</param>
+        void SetLanguageLine(string lineId, string text);
+
+        /// <summary>
+        /// Allows you to set the display name of a specific <see cref="TechType"/>.
+        /// </summary>
+        /// <param name="techType">The <see cref="TechType"/> whose display name that is to be changed.</param>
+        /// <param name="text">The new display name for the chosen <see cref="TechType"/>.</param>
+        void SetTechTypeName(TechType techType, string text);
+
+        /// <summary>
+        /// Allows you to set the tooltip of a specific <see cref="TechType"/>.
+        /// </summary>
+        /// <param name="techType">The <see cref="TechType"/> whose tooltip that is to be changed.</param>
+        /// <param name="text">The new tooltip for the chosen <see cref="TechType"/>.</param>
+        void SetTechTypeTooltip(TechType techType, string text);
+    }
+}
---
+++ SMLHelper/Interfaces/IPrefabHandler.cs
@@ -0,0 +1,14 @@
+﻿namespace SMLHelper.V2.Interfaces
+{
+    using Assets;
+
+    public interface IPrefabHandler
+    {
+        /// <summary>
+        /// Registers a ModPrefab into the game.
+        /// </summary>
+        /// <param name="prefab">The mod prefab to register. Create a child class inheriting off this one and configure as needed.</param>
+        /// <seealso cref="ModPrefab"/>
+        void RegisterPrefab(ModPrefab prefab);
+    }
+}
