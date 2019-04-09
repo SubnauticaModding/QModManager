f56ea892ff0309b30ac2b19de357e68b5a2131b6
AlexejheroYTB <alexejhero@gmail.com>
2019-04-09 18:12:35 +0300

    init

---
+++ SMLHelper/Patchers/TooltipPatcher.cs
@@ -0,0 +1,25 @@
+﻿namespace SMLHelper.V2.Patchers
+{
+    using Harmony;
+    using System;
+    using System.Reflection;
+
+    internal class TooltipPatcher
+    {
+        internal static void Patch(HarmonyInstance harmony)
+        {
+            Type TooltipFactoryType = typeof(TooltipFactory);
+            Type thisType = typeof(TooltipPatcher);
+            MethodInfo InventoryItemMethod = TooltipFactoryType.GetMethod("InventoryItem", BindingFlags.Public | BindingFlags.Static);
+            MethodInfo InventoryItemViewMethod = TooltipFactoryType.GetMethod("InventoryItemView", BindingFlags.Public | BindingFlags.Static);
+
+            harmony.Patch(InventoryItemMethod, new HarmonyMethod(thisType.GetMethod("II_Prefix", BindingFlags.NonPublic | BindingFlags.Static)));
+            harmony.Patch(InventoryItemViewMethod, new HarmonyMethod(thisType.GetMethod("IIV_Prefix", BindingFlags.NonPublic | BindingFlags.Static)));
+        }
+
+        internal static bool II_Prefix(InventoryItem item)
+        {
+            return false;
+        }
+    }
+}
