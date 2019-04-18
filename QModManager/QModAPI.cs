6a2271bebe1b533cf6a9bf333d35bff0d5915c3b
AlexejheroYTB <alexejhero@gmail.com>
2019-04-18 12:26:31 +0300

    QModAPI: GetAllMods

--- QModManager/QModAPI.cs
+++ QModManager/QModAPI.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Reflection;
 using System.Text.RegularExpressions;
 
@@ -15,29 +16,39 @@ namespace QModManager
             ErroredMods.Add(modAssembly);
         }
 
-        public static QMod GetMod(Assembly modAssembly = null)
+        public static ReadOnlyCollection<QMod> GetAllMods(bool includeUnloaded = false, bool includeErrored = false)
+        {
+            if (includeErrored)
+                return Patcher.foundMods.AsReadOnly();
+            else if (includeUnloaded)
+                return Patcher.sortedMods.AsReadOnly();
+            else
+                return Patcher.loadedMods.AsReadOnly();
+        }
+
+        public static QMod GetMod(Assembly modAssembly = null, bool includeUnloaded = false, bool includeErrored = false)
         {
             modAssembly = modAssembly ?? Assembly.GetCallingAssembly();
 
-            foreach (QMod mod in Patcher.sortedMods)
+            foreach (QMod mod in GetAllMods(includeUnloaded, includeErrored))
                 if (mod.LoadedAssembly == modAssembly) return mod;
             return null;
         }
-        public static QMod GetMod(string id)
+        public static QMod GetMod(string id, bool includeUnloaded = false, bool includeErrored = false)
         {
             if (string.IsNullOrEmpty(id)) return null;
-            foreach (QMod mod in Patcher.sortedMods)
+            foreach (QMod mod in GetAllMods(includeUnloaded, includeErrored))
                 if (mod.Id == Regex.Replace(id, "[^0-9a-z_]", "", RegexOptions.IgnoreCase)) return mod;
             return null;
         }
 
-        public static bool ModPresent(Assembly modAssembly)
+        public static bool ModPresent(Assembly modAssembly, bool includeUnloaded = false, bool includeErrored = false)
         {
-            return GetMod(modAssembly) != null;
+            return GetMod(modAssembly, includeUnloaded, includeErrored) != null;
         }
-        public static bool ModPresent(string id)
+        public static bool ModPresent(string id, bool includeUnloaded = false, bool includeErrored = false)
         {
-            return GetMod(id) != null;
+            return GetMod(id, includeUnloaded, includeErrored) != null;
         }
     }
 }
