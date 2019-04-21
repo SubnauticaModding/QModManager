9368debbfc3f767c4e1583ea8d56c31962c5972e
AlexejheroYTB <alexejhero@gmail.com>
2019-04-21 12:47:14 +0300

    Merge branch 'dev/master' into dev/dependency@versions

--- QModManager/Hooks.cs
+++ QModManager/Hooks.cs
@@ -9,6 +9,7 @@ namespace QModManager
     {
         public static Delegates.Start Start;
         public static Delegates.FixedUpdate FixedUpdate;
+        public static Delegates.LateStart LateStart;
         public static Delegates.Update Update;
         public static Delegates.LateUpdate LateUpdate;
         public static Delegates.OnApplicationQuit OnApplicationQuit;
@@ -16,6 +17,8 @@ namespace QModManager
         public static Delegates.SceneLoaded SceneLoaded;
 
         public static Delegates.OnLoadEnd OnLoadEnd;
+
+        public static bool LateStartInvoked { get; internal set; } = false;
 
         internal static void Load()
         {
@@ -25,9 +28,14 @@ namespace QModManager
         [HarmonyPatch(typeof(DevConsole), "Start")]
         internal static class AddComponentPatch
         {
+            internal static bool hooksLoaded = false;
+
             [HarmonyPostfix]
             internal static void Postfix(DevConsole __instance)
             {
+                if (hooksLoaded) return;
+                hooksLoaded = true;
+                
                 __instance.gameObject.AddComponent<QMMHooks>();
 
                 Logger.Debug("Hooks loaded");
@@ -38,7 +46,15 @@ namespace QModManager
 
         internal class QMMHooks : MonoBehaviour
         {
-            internal void FixedUpdate() => Hooks.FixedUpdate?.Invoke();
+            internal void FixedUpdate()
+            {
+                if (!LateStartInvoked)
+                {
+                    LateStart?.Invoke();
+                    LateStartInvoked = true;
+                }
+                Hooks.FixedUpdate?.Invoke();
+            }
             internal void Update() => Hooks.Update?.Invoke();
             internal void LateUpdate() => Hooks.LateUpdate?.Invoke();
             internal void OnApplicationQuit() => Hooks.OnApplicationQuit?.Invoke();
@@ -48,6 +64,7 @@ namespace QModManager
         {
             public delegate void Start();
             public delegate void FixedUpdate();
+            public delegate void LateStart();
             public delegate void Update();
             public delegate void LateUpdate();
             public delegate void OnApplicationQuit();
