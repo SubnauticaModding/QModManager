38cca5d93f9023c6863eb845eeec178858870376
AlexejheroYTB <alexejhero@gmail.com>
2019-05-14 20:56:49 +0300

    Create example mod

---
+++ Example mod/Class1.cs
@@ -0,0 +1,35 @@
+﻿using QModManager;
+using QModManager.API;
+using System;
+
+namespace Example_mod
+{
+    public static class Mod
+    {
+        public static void Patch()
+        {
+            Console.WriteLine("[TEST] Patched!");
+            QModHooks.OnLoadEnd += () =>
+            {
+                Console.WriteLine("[TEST] Preparing to send message");
+                QModAPI.SendMessage(QModAPI.GetMyMod(), "test", 1, true, "hello");
+                Console.WriteLine("[TEST] Message sent!");
+            };
+        }
+    }
+
+    public class MessageRec : MessageReceiver
+    {
+        public override IQMod From { get; } = QModAPI.GetMyMod();
+
+        public override void OnMessageReceived(IQMod from, string message, params object[] data)
+        {
+            Console.WriteLine("[TEST] Message received!");
+            Console.WriteLine(from.DisplayName);
+            Console.WriteLine(message);
+            Console.WriteLine(data[0]);
+            Console.WriteLine(data[1]);
+            Console.WriteLine(data[2]);
+        }
+    }
+}
