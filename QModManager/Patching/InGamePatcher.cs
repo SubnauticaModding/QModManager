242129f221e4dc8ce7f9f69a2aaa4b02bb049cab
AlexejheroYTB <alexejhero@gmail.com>
2020-01-06 10:29:40 +0200

    QoL - Disable errors from being sent out to the devs

--- QModManager/Patching/InGamePatcher.cs
+++ QModManager/Patching/InGamePatcher.cs
@@ -1,7 +1,8 @@
-﻿using Harmony;
-
-namespace QModManager.Patching
+﻿namespace QModManager.Patching
 {
+    using Harmony;
+    using UnityEngine;
+
     internal static class InGamePatcher
     {
         [HarmonyPatch(typeof(IngameMenu), nameof(IngameMenu.Update))]
@@ -15,6 +16,17 @@ namespace QModManager.Patching
                     IngameMenu.main.developerMode = OptionsManager.DevMode;
                     IngameMenu.main.developerButton.gameObject.SetActive(OptionsManager.DevMode);
                 }
+            }
+        }
+
+        [HarmonyPatch(typeof(SentrySdk), nameof(SentrySdk.Start))]
+        internal static class SentrySdk_Start_Patch
+        {
+            [HarmonyPrefix]
+            internal static bool Prefix(SentrySdk __instance)
+            {
+                GameObject.Destroy(__instance);
+                return false;
             }
         }
     }
