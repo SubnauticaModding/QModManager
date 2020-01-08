bd3fe738488f6bd4c66059a917da2a63a6cf0aef
AlexejheroYTB <alexejhero@gmail.com>
2020-01-09 00:54:04 +0200

    enable-achievements-merge - Merge Enable Achievements

---
+++ QModManager/HarmonyPatches/EnableAchievements.cs
@@ -0,0 +1,16 @@
+﻿// Original mod by AlexejheroYTB
+namespace QModManager.HarmonyPatches.EnableAchievements
+{
+    using Harmony;
+
+    [HarmonyPatch(typeof(GameAchievements), "Unlock")]
+    internal static class GameAchievements_Unlock
+    {
+        [HarmonyPrefix]
+        internal static bool Prefix(GameAchievements.Id id)
+        {
+            PlatformUtils.main.GetServices().UnlockAchievement(id);
+            return false;
+        }
+    }
+}
