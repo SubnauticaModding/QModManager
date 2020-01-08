f90fb356cbda446e7763c04bd5e50da5eef6f357
AlexejheroYTB <alexejhero@gmail.com>
2020-01-09 00:42:50 +0200

    quit-to-desktop-merge - Disable in hardcore

--- QModManager/HarmonyPatches/QuitToDesktop.cs
+++ QModManager/HarmonyPatches/QuitToDesktop.cs
@@ -1,28 +1,29 @@
-﻿namespace QModManager.HarmonyPatches.QuitToDesktop
+﻿// Original mod by RandyKnapp
+namespace QModManager.HarmonyPatches.QuitToDesktop
 {
     using Harmony;
+    using System.Collections;
     using UnityEngine;
     using UnityEngine.UI;
 
     [HarmonyPatch(typeof(IngameMenu), nameof(IngameMenu.Start))]
-    internal static class IngameMenu_Open_Patch
+    internal static class IngameMenu_Start
     {
+        internal static Button quitButton;
+        internal static GameObject quitConfirmation;
         internal static GameObject quitConfirmation2;
 
         [HarmonyPostfix]
         internal static void Postfix(IngameMenu __instance)
         {
-            __instance.quitToMainMenuText.text = "Quit to Main Menu";
-
             var buttonPrefab = __instance.quitToMainMenuButton.GetComponent<Button>();
-            var quitButton = GameObject.Instantiate(buttonPrefab, __instance.quitToMainMenuButton.transform.parent);
+            quitButton = GameObject.Instantiate(buttonPrefab, __instance.quitToMainMenuButton.transform.parent);
             quitButton.name = "QuitToDesktop Button";
             quitButton.onClick.RemoveAllListeners();
             quitButton.onClick.AddListener(() => QuitDesktopSubscreen(__instance));
-            quitButton.GetComponentsInChildren<Text>().Do(t => t.text = "Quit to Desktop");
 
             var confirmationPrefab = __instance.transform.Find("QuitConfirmation").gameObject;
-            var quitConfirmation = GameObject.Instantiate(confirmationPrefab, __instance.transform);
+            quitConfirmation = GameObject.Instantiate(confirmationPrefab, __instance.transform);
             quitConfirmation.name = "QuitToDesktop Confirmation";
             quitConfirmation.GetComponentsInChildren<Button>()[1].onClick.RemoveAllListeners();
             quitConfirmation.GetComponentsInChildren<Button>()[1].onClick.AddListener(() => __instance.QuitGame(true));
@@ -46,4 +47,46 @@
             __instance.ChangeSubscreen("QuitToDesktop Confirmation");
         }
     }
+
+    [HarmonyPatch(typeof(IngameMenu), nameof(IngameMenu.Open))]
+    internal static class IngameMenu_Open
+    {
+        [HarmonyPostfix]
+        internal static void Postfix(IngameMenu __instance)
+        {
+            __instance.quitToMainMenuText.text = $"{(GameModeUtils.IsPermadeath() ? "Save & " : "")}Quit to Main Menu";
+            IngameMenu_Start.quitButton.interactable = !GameModeUtils.IsPermadeath();
+            IngameMenu_Start.quitButton.GetComponentsInChildren<Text>().Do(t => t.text = "Quit to Desktop");
+        }
+    }
+
+    /*
+    [HarmonyPatch(typeof(IngameMenu), nameof(IngameMenu.QuitGame))]
+    internal static class IngameMenu_QuitGame
+    {
+        [HarmonyPrefix]
+        internal static bool Prefix(IngameMenu __instance, bool quitToDesktop)
+        {
+            __instance.StartCoroutine(IngameMenu_QuitGameAsync.Passthrough(__instance.QuitGameAsync(quitToDesktop), quitToDesktop));
+            return false;
+        }
+    }
+
+    internal static class IngameMenu_QuitGameAsync
+    {
+        internal static IEnumerator Passthrough(IEnumerator original, bool quitToDesktop)
+        {
+            int n = 0;
+            while (original.MoveNext())
+            {
+                yield return original.Current;
+
+                if (quitToDesktop && original.Current is IEnumerator)
+                    yield return new WaitForSecondsRealtime(2);
+
+                n++;
+            }
+        }
+    }
+    */
 }
