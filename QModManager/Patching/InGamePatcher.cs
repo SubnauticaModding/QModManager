cf951241f2f637259578774797c0dd072fd9cd1e
AlexejheroYTB <alexejhero@gmail.com>
2020-01-08 18:54:56 +0200

    QoL - Disable Give Feedback button while allowing the game to be paused with F8

--- QModManager/Patching/InGamePatcher.cs
+++ QModManager/Patching/InGamePatcher.cs
@@ -2,6 +2,7 @@
 {
     using Harmony;
     using UnityEngine;
+    using UnityEngine.UI;
 
     internal static class InGamePatcher
     {
@@ -16,6 +17,9 @@
                     IngameMenu.main.developerMode = OptionsManager.DevMode;
                     IngameMenu.main.developerButton.gameObject.SetActive(OptionsManager.DevMode);
                 }
+                __instance.feedbackButton.interactable = false;
+                Transform transform = __instance.transform.Find("Main/ButtonLayout/ButtonFeedback");
+                if (transform != null) transform.gameObject.GetComponent<Button>().interactable = false;
             }
         }
 
@@ -27,6 +31,17 @@
             {
                 GameObject.Destroy(__instance);
                 return false;
+            }
+        }
+
+        [HarmonyPatch(typeof(uGUI_FeedbackCollector), nameof(uGUI_FeedbackCollector.Update))]
+        internal static class uGUI_FeedbackCollector_Update_Patch
+        {
+            [HarmonyPostfix]
+            internal static void Postfix(uGUI_FeedbackCollector __instance)
+            {
+                if (__instance.repliesPanel.activeSelf) __instance.repliesPanel.SetActive(false);
+                if (__instance.root.activeSelf) __instance.root.SetActive(false);
             }
         }
     }
