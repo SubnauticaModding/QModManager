6575b3ca80a8d5ca9a4ff1aec4b94ba98ff12992
MrPurple6411 <mrpurple6411@gmail.com>
2020-01-22 20:31:36 -0700

    Working AudioFixer - Thanx NES

--- Executable/Properties/AssemblyInfo.cs
+++ Executable/Properties/AssemblyInfo.cs
@@ -12,5 +12,5 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("3.0.1")]
-[assembly: AssemblyFileVersion("3.0.1")]
+[assembly: AssemblyVersion("3.0.2")]
+[assembly: AssemblyFileVersion("3.0.2")]
