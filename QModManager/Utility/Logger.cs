af1454a3c252bd968fdf6cc15c7a65443f397e2e
O-S Marin <K07H@users.noreply.github.com>
2020-06-15 01:19:02 +0200

    Revert "Update Logger.cs"
    This reverts commit 874fd649e701d759a11e8a76be8225cb6c7c3da2.

--- QModManager/Utility/Logger.cs
+++ QModManager/Utility/Logger.cs
@@ -1,232 +1,127 @@
 ﻿namespace QModManager.Utility
 {
     using System;
+    using System.Diagnostics;
+    using System.IO;
 
-    /// <summary>A simple logging class. Can be used for basic logging or to know which logging level is enabled.</summary>
-    public static class Logger
+    internal static class Logger
     {
-        /// <summary>Possible logging levels.</summary>
-        public enum Level
+        internal enum Level
         {
-            /// <summary>Debugging log level</summary>
             Debug,
-            /// <summary>Informational log level</summary>
             Info,
-            /// <summary>Warning log level</summary>
             Warn,
-            /// <summary>Error log level</summary>
             Error,
-            /// <summary>Fatal log level</summary>
             Fatal
         }
 
-        private const string AssemblyName = "QModManager";
-
-        #region Private functions (Used by the Logger)
-
-        private static string GetCallingAssemblyName() => ReflectionHelper.CallingAssemblyByStackTrace()?.GetName().Name;
-
-        private static void Debug(string msg, bool showOnScreen = false, string callingAssembly = null, bool force = false)
+        private static void Log(string logLevel, params string[] text)
         {
-            if (!force && !Config.EnableDebugLogs)
+            if (text == null || text.Length < 1)
                 return;
 
-            Console.WriteLine($"[{callingAssembly ?? GetCallingAssemblyName()}:DEBUG] {msg}");
+            string from;
+            Type classType = GetCallingClass();
 
-            if (showOnScreen)
-                ErrorMessage.AddDebug(msg);
+            if (classType == null)
+                from = null;
+            else if (classType.Namespace.Contains("SMLHelper"))
+                from = "SMLHelper";
+            else
+                from = classType.Name;
+
+            string toWrite = "[QModManager] ";
+            if (!string.IsNullOrEmpty(from) && !from.Contains("<>"))
+                toWrite += $"[{from}] ";
+            else if (!string.IsNullOrEmpty(from) && from.Contains("<>"))
+                toWrite += $"[Anonymous] ";
+            if (!string.IsNullOrEmpty(logLevel))
+                toWrite += $"[{logLevel}] ";
+
+            int length = toWrite.Length;
+
+            Console.WriteLine($"{toWrite}{text[0]}");
+
+            for (int i = 1; i < text.Length; i++)
+                Console.WriteLine($"{text[i]}");
         }
 
-        private static void Debug(bool selfAssembly = false, params string[] msgs)
+        internal static void Log(params string[] text)
         {
-            if (Config.EnableDebugLogs && msgs != null && msgs.Length > 0)
-            {
-                string callingAssembly = selfAssembly ? AssemblyName : GetCallingAssemblyName();
-                foreach (string msg in msgs)
-                    Debug(msg, false, callingAssembly, false);
-            }
+            Log("", text);
         }
 
-        private static void DebugForce(bool selfAssembly = false, params string[] msgs)
-        {
-            if (msgs != null && msgs.Length > 0)
-            {
-                string callingAssembly = selfAssembly ? AssemblyName : GetCallingAssemblyName();
-                foreach (string msg in msgs)
-                    Debug(msg, false, callingAssembly, true);
-            }
-        }
-
-        private static void Info(string msg, bool showOnScreen = false, string callingAssembly = null)
-        {
-            Console.WriteLine($"[{callingAssembly ?? GetCallingAssemblyName()}:INFO] {msg}");
-
-            if (showOnScreen)
-                ErrorMessage.AddMessage(msg);
-        }
-
-        private static void Info(bool selfAssembly = false, params string[] msgs)
-        {
-            if (msgs != null && msgs.Length > 0)
-            {
-                string callingAssembly = selfAssembly ? AssemblyName : GetCallingAssemblyName();
-                foreach (string msg in msgs)
-                    Info(msg, false, callingAssembly);
-            }
-        }
-
-        private static void Warn(string msg, bool showOnScreen = false, string callingAssembly = null)
-        {
-            Console.WriteLine($"[{callingAssembly ?? GetCallingAssemblyName()}:WARN] {msg}");
-
-            if (showOnScreen)
-                ErrorMessage.AddWarning(msg);
-        }
-
-        private static void Warn(bool selfAssembly = false, params string[] msgs)
-        {
-            if (msgs != null && msgs.Length > 0)
-            {
-                string callingAssembly = selfAssembly ? AssemblyName : GetCallingAssemblyName();
-                foreach (string msg in msgs)
-                    Warn(msg, false, callingAssembly);
-            }
-        }
-
-        private static void Error(string msg = null, Exception ex = null, bool showOnScreen = false, string callingAssembly = null)
-        {
-            if (ex != null)
-                msg = (string.IsNullOrEmpty(msg) ? ex.ToString() : msg + Environment.NewLine + ex.ToString());
-
-            Console.WriteLine($"[{callingAssembly ?? GetCallingAssemblyName()}:ERROR] {msg}");
-
-            if (showOnScreen && !string.IsNullOrEmpty(msg))
-                ErrorMessage.AddError(msg);
-        }
-
-        private static void Error(bool selfAssembly = false, params string[] msgs)
-        {
-            if (msgs != null && msgs.Length > 0)
-            {
-                string callingAssembly = selfAssembly ? AssemblyName : GetCallingAssemblyName();
-                foreach (string msg in msgs)
-                    Error(msg, null, false, callingAssembly);
-            }
-        }
-
-        private static void Exception(Exception e, bool selfAssembly = false) => Error(null, e, false, selfAssembly ? AssemblyName : GetCallingAssemblyName());
-
-        private static void Fatal(string msg = null, Exception ex = null, bool showOnScreen = false, string callingAssembly = null)
-        {
-            if (ex != null)
-                msg = (string.IsNullOrEmpty(msg) ? ex.ToString() : msg + Environment.NewLine + ex.ToString());
-            Console.WriteLine($"[{callingAssembly ?? GetCallingAssemblyName()}:FATAL] {msg}");
-
-            if (showOnScreen && !string.IsNullOrEmpty(msg))
-                ErrorMessage.AddError(msg);
-        }
-
-        private static void Fatal(bool selfAssembly = false, params string[] msgs)
-        {
-            if (msgs != null && msgs.Length > 0)
-            {
-                string callingAssembly = selfAssembly ? AssemblyName : GetCallingAssemblyName();
-                foreach (string msg in msgs)
-                    Fatal(msg, null, false, callingAssembly);
-            }
-        }
-
-        #endregion
-
-        #region Internal functions (used by QModManager)
-
-        internal static void Debug(params string[] msgs) => Debug(true, msgs);
-
-        internal static void DebugForce(params string[] msgs) => DebugForce(true, msgs);
-
-        internal static void Info(params string[] msgs) => Info(true, msgs);
-
-        internal static void Warn(params string[] msgs) => Warn(true, msgs);
-
-        internal static void Error(params string[] msgs) => Error(true, msgs);
-
-        internal static void Exception(Exception e) => Exception(e, true);
-
-        internal static void Fatal(params string[] msgs) => Fatal(true, msgs);
-
-        #endregion
-
-        #region Public functions (used by mods and QModManager)
-
-        /// <summary>Used to know if debug logging is enabled or not.</summary>
-        public static bool DebugLogsEnabled => Config.EnableDebugLogs;
-
-        /// <summary>
-        /// This function will log given message and/or exception. It can optionally show the message on screen.
-        /// Note: You need to provide a message and/or an exception (this function will do nothing if both are set to null).
-        /// </summary>
-        /// <param name="logLevel">The level of the log.</param>
-        /// <param name="msg">Optional: The message that needs to be logged.</param>
-        /// <param name="ex">Optional: The exception that needs to be logged.</param>
-        /// <param name="showOnScreen">Optional: Whether to show the message on screen or not.</param>
-        public static void Log(Level logLevel, string msg = null, Exception ex = null, bool showOnScreen = false)
-        {
-            if (ex != null)
-            {
-                // If exception was provided, concatenate its message to the log message
-                if (logLevel != Level.Error && logLevel != Level.Fatal)
-                    msg = (msg == null) ? ex.Message : msg + Environment.NewLine + ex.Message;
-            }
-            else if (msg == null) // Return if both given message and exception were null
-                return;
-
-            switch (logLevel)
-            {
-                case Level.Debug:
-                    Debug(msg, showOnScreen);
-                    break;
-                case Level.Warn:
-                    Warn(msg, showOnScreen);
-                    break;
-                case Level.Error:
-                    Error(msg, ex, showOnScreen);
-                    break;
-                case Level.Fatal:
-                    Fatal(msg, ex, showOnScreen);
-                    break;
-                default: // Defaults to informational logging
-                    Info(msg, showOnScreen);
-                    break;
-            }
-        }
-
-        /// <summary>
-        /// This function will log given array of strings (each string of the array is logged as an individual log line).
-        /// </summary>
-        /// <param name="logLevel">The level of the log.</param>
-        /// <param name="text">The array of strings that needs to be logged (each string will be logged individually).</param>
-        public static void Log(Level logLevel, params string[] text)
+        internal static void Log(Level logLevel, params string[] text)
         {
             switch (logLevel)
             {
                 case Level.Debug:
-                    Debug(false, text);
+                    Debug(text);
+                    break;
+                case Level.Info:
+                    Info(text);
                     break;
                 case Level.Warn:
-                    Warn(false, text);
+                    Warn(text);
                     break;
                 case Level.Error:
-                    Error(false, text);
+                    Error(text);
                     break;
                 case Level.Fatal:
-                    Fatal(false, text);
-                    break;
-                default: // Defaults to informational logging
-                    Info(false, text);
+                    Fatal(text);
                     break;
             }
         }
 
-        #endregion
+        internal static void Debug(params string[] text)
+        {
+            if (Config.EnableDebugLogs)
+                Log("Debug", text);
+        }
+
+        internal static void DebugForce(params string[] text)
+        {
+            Log("Debug", text);
+        }
+
+        internal static void Info(params string[] text)
+        {
+            Log("Info", text);
+        }
+
+        internal static void Warn(params string[] text)
+        {
+            Log("Warn", text);
+        }
+
+        internal static void Error(params string[] text)
+        {
+            Log("Error", text);
+        }
+
+        internal static void Exception(Exception e)
+        {
+            Log("Exception", e.ToString());
+        }
+
+        internal static void Fatal(params string[] text)
+        {
+            Log("Fatal", text);
+        }
+
+        private static Type GetCallingClass()
+        {
+            var stackTrace = new StackTrace();
+            StackFrame[] frames = stackTrace.GetFrames();
+
+            foreach (StackFrame stackFrame in frames)
+            {
+                Type declaringClass = stackFrame.GetMethod().DeclaringType;
+                if (declaringClass != typeof(Logger))
+                    return declaringClass;
+            }
+
+            return null;
+        }
     }
 }
