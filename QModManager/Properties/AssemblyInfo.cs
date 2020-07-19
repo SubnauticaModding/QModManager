e39eb47bc33d43c7b2fffb836549310495da822f
Tobey Blaber <tobey@straitjacket.software>
2020-07-19 18:19:53 +0100

    Bumping assembly version numbers to 4.0.0.0

--- QModManager/Properties/AssemblyInfo.cs
+++ QModManager/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("QModManager 3")]
+[assembly: AssemblyTitle("QModManager 4")]
 [assembly: AssemblyDescription("Config based patch management for Subnautica and Subnautica: Below Zero")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Subnautica Modding")]
@@ -13,8 +13,8 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("3.3.0.0")]
-[assembly: AssemblyFileVersion("3.3.0.0")]
+[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.0.0.0")]
 
 [assembly: InternalsVisibleTo("QMMTests")]
 [assembly: InternalsVisibleTo("QModManager")]
