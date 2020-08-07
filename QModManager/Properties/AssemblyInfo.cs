336b8b9cfe8606066dfb5eda47eccf61cdc6deeb
Tobey Blaber <tobey@straitjacket.software>
2020-08-07 21:05:54 +0100

    Version bump to 4.0.2.1, Build and package for Vortex

--- QModManager/Properties/AssemblyInfo.cs
+++ QModManager/Properties/AssemblyInfo.cs
@@ -13,8 +13,8 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("4.0.2.0")]
-[assembly: AssemblyFileVersion("4.0.2.0")]
+[assembly: AssemblyVersion("4.0.2.1")]
+[assembly: AssemblyFileVersion("4.0.2.1")]
 
 [assembly: InternalsVisibleTo("QMMTests")]
 [assembly: InternalsVisibleTo("QModManager")]
