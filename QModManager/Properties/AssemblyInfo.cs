e7185b16a8c3d9e697781c9b5126e437f977d936
Tobey Blaber <tobey@straitjacket.software>
2020-09-16 22:04:34 +0100

    Change version to 4.0.2.4

--- QModManager/Properties/AssemblyInfo.cs
+++ QModManager/Properties/AssemblyInfo.cs
@@ -13,8 +13,8 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("4.0.3.0")]
-[assembly: AssemblyFileVersion("4.0.3.0")]
+[assembly: AssemblyVersion("4.0.2.4")]
+[assembly: AssemblyFileVersion("4.0.2.4")]
 
 [assembly: InternalsVisibleTo("QMMTests")]
 [assembly: InternalsVisibleTo("QModManager")]
