5eeea68f1b0f46d88cb5d7823fa22af3d1335d8d
MrPurple6411 <mrpurple6411@gmail.com>
2021-05-08 13:25:55 -0600

    Version Bump

--- Executable/Properties/AssemblyInfo.cs
+++ Executable/Properties/AssemblyInfo.cs
@@ -12,5 +12,5 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("4.1.1")]
-[assembly: AssemblyFileVersion("4.1.1")]
+[assembly: AssemblyVersion("4.1.2")]
+[assembly: AssemblyFileVersion("4.1.2")]
