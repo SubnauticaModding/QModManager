831dce9fd24bee33f37b6237aa2d4e5349a36d96
MrPurple6411 <mrpurple6411@gmail.com>
2021-05-16 14:06:37 -0600

    Version Bump

--- QModManager/Properties/AssemblyInfo.cs
+++ QModManager/Properties/AssemblyInfo.cs
@@ -13,8 +13,8 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("4.1.2")]
-[assembly: AssemblyFileVersion("4.1.2")]
+[assembly: AssemblyVersion("4.1.3")]
+[assembly: AssemblyFileVersion("4.1.3")]
 
 [assembly: InternalsVisibleTo("QMMTests")]
 [assembly: InternalsVisibleTo("QModManager")]
