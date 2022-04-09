703f4eceaf91027879799c77993fc4623f7c6137
MrPurple6411 <mrpurple6411@gmail.com>
2022-04-08 22:02:00 -0600

    Ugly yet functional Reflection fix for Extra Conditional parameter

--- QModManager/OptionsManager.cs
+++ QModManager/OptionsManager.cs
@@ -1,7 +1,10 @@
 ﻿namespace QModManager
 {
     using HarmonyLib;
+    using QModManager.API;
+    using QModManager.Patching;
     using QModManager.Utility;
+    using System.Reflection;
     using UnityEngine.Events;
 
     internal static class OptionsManager
@@ -17,10 +20,14 @@
                 ModsTab = __instance.AddTab("Mods");
                 __instance.AddHeading(ModsTab, "QModManager");
 
-                //TODO: This is the Last berrier to unifying the 2 games installs.....  Below Zero has an optional value at the end of AddToggleOption that makes it so these fail when using a binary compiled on the wrong game.
-                __instance.AddToggleOption(ModsTab, "Check for updates", Config.CheckForUpdates, new UnityAction<bool>(value => Config.CheckForUpdates = value));
+                
+                MethodInfo AddToggleOption = null; 
+                if(Patcher.CurrentlyRunningGame == QModGame.Subnautica)
+                {
+                    AddToggleOption = typeof(uGUI_OptionsPanel).GetMethod(nameof(AddToggleOption), new System.Type[] { typeof(int), typeof(string), typeof(bool), typeof(UnityAction<bool>) });
+                    AddToggleOption.Invoke(__instance, new object[] { ModsTab, "Check for updates", Config.CheckForUpdates, new UnityAction<bool>(value => Config.CheckForUpdates = value) });
 
-                __instance.AddToggleOption(ModsTab, "Enable console", Config.EnableConsole, new UnityAction<bool>(value =>
+                    AddToggleOption.Invoke(__instance, new object[] { ModsTab, "Enable console", Config.EnableConsole, new UnityAction<bool>(value =>
                 {
                     Config.EnableConsole = value;
 #if SUBNAUTICA_STABLE
@@ -29,11 +36,31 @@
 #elif BELOWZERO || SUBNAUTICA_EXP
                     PlatformUtils.SetDevToolsEnabled(value);
 #endif
-                }));
+                }) });
 
-                __instance.AddToggleOption(ModsTab, "Enable debug logs", Config.EnableDebugLogs, new UnityAction<bool>(value => Config.EnableDebugLogs = value));
+                    AddToggleOption.Invoke(__instance, new object[] { ModsTab, "Enable debug logs", Config.EnableDebugLogs, new UnityAction<bool>(value => Config.EnableDebugLogs = value) });
+                    AddToggleOption.Invoke(__instance, new object[] { ModsTab, "Enable developer mode", Config.EnableDevMode, new UnityAction<bool>(value => Config.EnableDevMode = value) });
+                }
+                else
+                {
+                    AddToggleOption = typeof(uGUI_OptionsPanel).GetMethod(nameof(AddToggleOption), new System.Type[] { typeof(int), typeof(string), typeof(bool), typeof(UnityAction<bool>), typeof(string) });
+                    AddToggleOption.Invoke(__instance, new object[] { ModsTab, "Check for updates", Config.CheckForUpdates, new UnityAction<bool>(value => Config.CheckForUpdates = value), null });
 
-                __instance.AddToggleOption(ModsTab, "Enable developer mode", Config.EnableDevMode, new UnityAction<bool>(value => Config.EnableDevMode = value));
+                    AddToggleOption.Invoke(__instance, new object[] { ModsTab, "Enable console", Config.EnableConsole, new UnityAction<bool>(value =>
+                {
+                    Config.EnableConsole = value;
+#if SUBNAUTICA_STABLE
+                    DevConsole.disableConsole = !value;
+                    UnityEngine.PlayerPrefs.SetInt("UWE.DisableConsole", value ? 0 : 1);
+#elif BELOWZERO || SUBNAUTICA_EXP
+                    PlatformUtils.SetDevToolsEnabled(value);
+#endif
+                }), null });
+
+                    AddToggleOption.Invoke(__instance, new object[] { ModsTab, "Enable debug logs", Config.EnableDebugLogs, new UnityAction<bool>(value => Config.EnableDebugLogs = value), null });
+                    AddToggleOption.Invoke(__instance, new object[] { ModsTab, "Enable developer mode", Config.EnableDevMode, new UnityAction<bool>(value => Config.EnableDevMode = value), null });
+                }
+
             }
         }
     }
